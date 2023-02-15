082bfbe5021642d5b7a9d4efcc77730cd85afdb2
Александр Морев <a.morev@kryptonite.ru>
2023-02-15 18:30:47 +0300

    remove unused imports

--- single-number-i/test.cpp
+++ single-number-i/test.cpp
@@ -2,8 +2,6 @@
 
 #include <solution.hpp>
 
-#include <vector>
-
 TEST_CASE("Simple") {
   { REQUIRE(1 == Solution::singleNumber({2, 2, 1})); }
   { REQUIRE(4 == Solution::singleNumber({4, 1, 2, 1, 2})); }
--- single-number-ii/test.cpp
+++ single-number-ii/test.cpp
@@ -2,8 +2,6 @@
 
 #include <solution.hpp>
 
-#include <vector>
-
 TEST_CASE("Simple") {
   { REQUIRE(3 == Solution::singleNumber({2, 2, 3, 2})); }
   { REQUIRE(99 == Solution::singleNumber({0, 1, 0, 1, 0, 1, 99})); }
