04ec427dc906435a4526512985d4c56da3da1009
Александр Морев <a.morev@kryptonite.ru>
2023-02-17 14:36:03 +0300

    Minimum Distance Between BST Nodes

---
+++ minimum-distance-between-bst-nodes/solution.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <tree_node.h>
+
+#include <algorithm>
+#include <limits>
+#include <optional>
+
+class Solution {
+public:
+  static int minDiffInBST(TreeNode *root) {
+    int diff = std::numeric_limits<int>::max();
+    std::optional<int> previous = std::nullopt;
+    inOrderTraversal(root, diff, previous);
+    return diff;
+  }
+
+private:
+  static void inOrderTraversal(TreeNode *root, int &diff,
+                               std::optional<int> &previous) {
+    if (!root) {
+      return;
+    }
+
+    inOrderTraversal(root->left, diff, previous);
+    if (previous) {
+      diff = std::min(diff, root->val - previous.value());
+    }
+    previous = root->val;
+    inOrderTraversal(root->right, diff, previous);
+  }
+};
---
+++ minimum-distance-between-bst-nodes/test.cpp
@@ -0,0 +1,28 @@
+#include <catch.hpp>
+#include <tree_node.h>
+
+#include <solution.hpp>
+
+#include <optional>
+#include <vector>
+
+void CheckSolution(const std::vector<std::optional<int>> &values,
+                   int max_diff) {
+  auto root = VectorToTree(values);
+
+  REQUIRE(max_diff == Solution::minDiffInBST(root));
+
+  FreeTree(root);
+}
+
+TEST_CASE("Simple") {
+  {
+    std::vector<std::optional<int>> values{4, 2, 6, 1, 3};
+    CheckSolution(values, 1);
+  }
+  {
+    std::vector<std::optional<int>> values{
+        1, 0, 48, std::nullopt, std::nullopt, 12, 49};
+    CheckSolution(values, 1);
+  }
+}
