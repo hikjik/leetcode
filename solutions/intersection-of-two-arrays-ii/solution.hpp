973ba39a6da6ff0ae3474335540b54373c1f9371
Александр Морев <a.morev@kryptonite.ru>
2023-02-18 03:57:19 +0300

    Intersection of Two Arrays II

---
+++ solutions/intersection-of-two-arrays-ii/solution.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int> intersect(const std::vector<int> &nums1,
+                                    const std::vector<int> &nums2) {
+    std::unordered_map<int, int> counter;
+    for (auto num : nums1) {
+      counter[num]++;
+    }
+
+    std::vector<int> intersection;
+    for (auto num : nums2) {
+      if (counter[num]) {
+        counter[num]--;
+        intersection.push_back(num);
+      }
+    }
+    return intersection;
+  }
+};
