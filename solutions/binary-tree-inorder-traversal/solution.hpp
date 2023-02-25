38c123080be722c743e660e6b6c287daa5f11953
Александр Морев <a.morev@kryptonite.ru>
2023-02-25 14:49:04 +0300

    Binary Tree Inorder Traversal

---
+++ solutions/binary-tree-inorder-traversal/solution.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <tree_node.h>
+
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int> inorderTraversal(TreeNode *root) {
+    std::vector<int> values;
+    traverseInOrder(root, &values);
+    return values;
+  }
+
+private:
+  static void traverseInOrder(TreeNode *root, std::vector<int> *values) {
+    if (!root) {
+      return;
+    }
+
+    traverseInOrder(root->left, values);
+    values->push_back(root->val);
+    traverseInOrder(root->right, values);
+  }
+};
