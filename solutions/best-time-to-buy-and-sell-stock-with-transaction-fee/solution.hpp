4a3932ce2266f78734c604283319eeef4f100f30
Александр Морев <a.morev@kryptonite.ru>
2023-02-26 23:50:43 +0300

    Optimize sell stocks

--- solutions/best-time-to-buy-and-sell-stock-with-transaction-fee/solution.hpp
+++ solutions/best-time-to-buy-and-sell-stock-with-transaction-fee/solution.hpp
@@ -7,14 +7,14 @@ public:
   static int maxProfit(std::vector<int> &prices, int fee) {
     size_t n = prices.size();
 
-    std::vector<std::vector<int>> dp(prices.size(), std::vector<int>(2, 0));
-    dp[0][1] = -prices[0];
+    std::vector<std::vector<int>> dp(2, std::vector<int>(n, 0));
+    dp[1][0] = -prices[0];
 
-    for (size_t i = 1; i < prices.size(); ++i) {
-      dp[i][0] = std::max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
-      dp[i][1] = std::max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+    for (size_t i = 1; i < n; ++i) {
+      dp[0][i] = std::max(dp[0][i - 1], dp[1][i - 1] + prices[i] - fee);
+      dp[1][i] = std::max(dp[1][i - 1], dp[0][i - 1] - prices[i]);
     }
 
-    return dp[n - 1][0];
+    return dp[0][n - 1];
   }
 };
