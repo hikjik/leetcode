1f5da04ae1ebd1cfecbe43eb02efbff1256d7f65
Александр Морев <a.morev@kryptonite.ru>
2023-02-28 23:59:59 +0300

    Search in a Binary Search Tree

---
+++ solutions/search-in-a-binary-search-tree/solution.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <tree_node.h>
+
+class Solution {
+public:
+  static TreeNode *searchBST(TreeNode *root, int val) {
+    if (!root || root->val == val) {
+      return root;
+    }
+
+    return val < root->val ? searchBST(root->left, val)
+                           : searchBST(root->right, val);
+  }
+};
---
+++ solutions/search-in-a-binary-search-tree/test.cpp
@@ -0,0 +1,22 @@
+#include <catch.hpp>
+#include <tree_node.h>
+
+#include <solution.hpp>
+
+#include <optional>
+#include <vector>
+
+TEST_CASE("Simple") {
+  {
+    std::vector<std::optional<int>> values{4, 2, 7, 1, 3};
+    auto root = VectorToTree(values);
+
+    auto find = Solution::searchBST(root, 2);
+    REQUIRE(find);
+    REQUIRE(2 == find->val);
+
+    REQUIRE_FALSE(Solution::searchBST(root, 5));
+
+    FreeTree(root);
+  }
+}
