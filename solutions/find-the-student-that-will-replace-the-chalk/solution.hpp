2d79838ca4070367094d4c012f6f10e588b85311
Александр Морев <hikjik@yandex.ru>
2023-03-07 02:55:54 +0300

    Find the Student that Will Replace the Chalk

---
+++ solutions/find-the-student-that-will-replace-the-chalk/solution.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <numeric>
+#include <vector>
+
+class Solution {
+public:
+  static int chalkReplacer(const std::vector<int> &chalk, int k) {
+    k %= std::accumulate(chalk.begin(), chalk.end(), 0L);
+
+    int i = 0;
+    while (k >= chalk[i]) {
+      k -= chalk[i++];
+    }
+    return i;
+  }
+};
