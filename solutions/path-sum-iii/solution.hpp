41e2a3826706a0783d70a59bbc1027158d1d6931
Александр Морев <hikjik@yandex.ru>
2023-03-10 00:33:12 +0300

    Path Sum III

--- solutions/path-sum-iii/solution.hpp
+++ solutions/path-sum-iii/solution.hpp
@@ -1,5 +1,26 @@
 #pragma once
 
+#include <tree_node.h>
+
+#include <vector>
+
 class Solution {
 public:
+  static int pathSum(TreeNode *root, int target) {
+    if (!root) {
+      return 0;
+    }
+    return continuousPathSum(root, target) + pathSum(root->left, target) +
+           pathSum(root->right, target);
+  }
+
+private:
+  static int continuousPathSum(TreeNode *root, long long target) {
+    if (!root) {
+      return 0;
+    }
+    return (root->val == target) +
+           continuousPathSum(root->left, target - root->val) +
+           continuousPathSum(root->right, target - root->val);
+  }
 };
--- solutions/path-sum-iii/test.cpp
+++ solutions/path-sum-iii/test.cpp
@@ -1,5 +1,50 @@
 #include <catch.hpp>
+#include <tree_node.h>
 
 #include <solution.hpp>
 
-TEST_CASE("Simple") {}
+#include <optional>
+#include <vector>
+
+void CheckSolution(const std::vector<std::optional<int>> &values, int target,
+                   int cnt) {
+  auto root = VectorToTree(values);
+  REQUIRE(cnt == Solution::pathSum(root, target));
+  FreeTree(root);
+}
+
+TEST_CASE("Simple") {
+  {
+    int target = 8;
+    int cnt = 3;
+    std::vector<std::optional<int>> values{
+        10, 5, -3, 3, 2, std::nullopt, 11, 3, -2, std::nullopt, 1};
+    CheckSolution(values, target, cnt);
+  }
+  {
+    int target = 22;
+    std::vector<std::optional<int>> values{
+        5, 4, 8, 11, std::nullopt, 13, 4, 7, 2, std::nullopt, std::nullopt,
+        5, 1};
+    int cnt = 3;
+    CheckSolution(values, target, cnt);
+  }
+  {
+    int target = 0;
+    std::vector<std::optional<int>> values{1};
+    int cnt = 0;
+    CheckSolution(values, target, cnt);
+  }
+  {
+    int target = 1;
+    std::vector<std::optional<int>> values{1};
+    int cnt = 1;
+    CheckSolution(values, target, cnt);
+  }
+  {
+    int target = 0;
+    std::vector<std::optional<int>> values;
+    int cnt = 0;
+    CheckSolution(values, target, cnt);
+  }
+}
