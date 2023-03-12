063b4216c914ca341732ed93e3edda3b51916b50
Александр Морев <hikjik@yandex.ru>
2023-03-12 13:43:20 +0300

    H-Index

---
+++ solutions/h-index/solution.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+  static int hIndex(std::vector<int> citations) {
+    int n = citations.size();
+    std::sort(citations.begin(), citations.end());
+
+    int left = 0, right = n;
+    while (left < right) {
+      const auto middle = left + (right - left) / 2;
+      if (citations[middle] >= n - middle) {
+        right = middle;
+      } else {
+        left = middle + 1;
+      }
+    }
+    return n - left;
+  }
+};
