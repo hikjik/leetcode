35a28e518949fb3ce17796153d1592c77ed20aae
Александр Морев <hikjik@yandex.ru>
2023-03-13 23:17:31 +0300

    Optimize Repeated DNA Sequences

--- solutions/repeated-dna-sequences/solution.hpp
+++ solutions/repeated-dna-sequences/solution.hpp
@@ -1,19 +1,56 @@
 #pragma once
 
+#include <bitset>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 class Solution {
 public:
   static std::vector<std::string> findRepeatedDnaSequences(std::string s) {
-    std::vector<std::string> repeated;
-    std::unordered_map<std::string, int> counter;
-    for (size_t i = 0; i + 9 < s.size(); ++i) {
-      if (++counter[s.substr(i, 10)] == 2) {
-        repeated.push_back(s.substr(i, 10));
-      }
+    if (s.size() < 10) {
+      return {};
     }
-    return repeated;
+
+    std::vector<std::string> repeated_sequences;
+    std::bitset<1 << 20> repeated;
+    std::bitset<1 << 20> seen;
+
+    int hash = 0;
+    for (size_t i = 0; i < 9; ++i) {
+      hash = (hash << 2) | convert(s[i]);
+    }
+
+    for (size_t i = 9; i < s.size(); ++i) {
+      hash = (hash << 2) | convert(s[i]);
+      hash &= 0xFFFFF;
+
+      if (repeated[hash]) {
+        continue;
+      }
+
+      if (seen[hash]) {
+        repeated_sequences.push_back(s.substr(i - 9, 10));
+        repeated.set(hash);
+      }
+      seen.set(hash);
+    }
+
+    return repeated_sequences;
+  }
+
+private:
+  static int convert(char c) {
+    switch (c) {
+    case 'A':
+      return 0;
+    case 'C':
+      return 1;
+    case 'G':
+      return 2;
+    case 'T':
+      return 3;
+    default:
+      throw;
+    }
   }
 };
