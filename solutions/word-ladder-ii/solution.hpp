fff7278559ba788ef0ca1dce66f2eb26ef7a14b7
Александр Морев <hikjik@yandex.ru>
2023-03-13 04:58:50 +0300

    Word Ladder II

---
+++ solutions/word-ladder-ii/solution.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <queue>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<std::vector<std::string>>
+  findLadders(std::string begin_word, std::string end_word,
+              std::vector<std::string> &word_list) {
+    std::unordered_set<std::string> words{word_list.begin(), word_list.end()};
+    if (!words.count(end_word)) {
+      return {};
+    }
+
+    std::vector<std::vector<std::string>> levels{{begin_word}};
+    words.erase(begin_word);
+
+    while (words.count(end_word)) {
+      levels.push_back(getNextLevel(levels.back(), &words));
+      if (levels.back().empty()) {
+        return {};
+      }
+    }
+
+    std::vector<std::vector<std::string>> paths;
+    std::vector<std::string> path{end_word};
+    dfs(levels.size() - 2, levels, &path, &paths);
+    return paths;
+  }
+
+private:
+  static void dfs(int i, const std::vector<std::vector<std::string>> &levels,
+                  std::vector<std::string> *path,
+                  std::vector<std::vector<std::string>> *paths) {
+    if (i < 0) {
+      paths->emplace_back(path->rbegin(), path->rend());
+      return;
+    }
+
+    const auto s = path->back();
+    for (const auto &p : levels[i]) {
+      if (isAdjacent(s, p)) {
+        path->push_back(p);
+        dfs(i - 1, levels, path, paths);
+        path->pop_back();
+      }
+    }
+  }
+
+  static std::vector<std::string>
+  getNextLevel(const std::vector<std::string> &prev_level,
+               std::unordered_set<std::string> *words) {
+    std::vector<std::string> level;
+    for (const auto &s : prev_level) {
+      for (auto it = words->begin(); it != words->end();) {
+        if (isAdjacent(s, *it)) {
+          level.push_back(*it);
+          it = words->erase(it);
+        } else {
+          ++it;
+        }
+      }
+    }
+    return level;
+  }
+
+  static bool isAdjacent(const std::string &s, const std::string &p) {
+    int cnt = 0;
+    for (size_t i = 0; i < s.size(); ++i) {
+      cnt += s[i] != p[i];
+    }
+    return cnt == 1;
+  }
+};
---
+++ solutions/word-ladder-ii/test.cpp
@@ -0,0 +1,124 @@
+#include <catch.hpp>
+
+#include <solution.hpp>
+
+#include <algorithm>
+
+TEST_CASE("Simple") {
+  {
+    std::string begin_word = "hit";
+    std::string end_word = "hit";
+    std::vector<std::string> word_list{"hit"};
+    std::vector<std::vector<std::string>> expected{{"hit"}};
+    auto actual = Solution::findLadders(begin_word, end_word, word_list);
+    std::sort(actual.begin(), actual.end());
+    REQUIRE(expected == actual);
+  }
+  {
+    std::string begin_word = "hit";
+    std::string end_word = "cog";
+    std::vector<std::string> word_list{"hot", "dot", "dog",
+                                       "lot", "log", "cog"};
+    std::vector<std::vector<std::string>> expected{
+        {"hit", "hot", "dot", "dog", "cog"},
+        {"hit", "hot", "lot", "log", "cog"}};
+    auto actual = Solution::findLadders(begin_word, end_word, word_list);
+    std::sort(actual.begin(), actual.end());
+    REQUIRE(expected == actual);
+  }
+  {
+    std::string begin_word = "hit";
+    std::string end_word = "cog";
+    std::vector<std::string> word_list{"hot", "dot", "dog", "lot", "log"};
+    std::vector<std::vector<std::string>> expected;
+    auto actual = Solution::findLadders(begin_word, end_word, word_list);
+    std::sort(actual.begin(), actual.end());
+    REQUIRE(expected == actual);
+  }
+  {
+    std::string begin_word = "red";
+    std::string end_word = "tax";
+    std::vector<std::string> word_list{"ted", "tex", "red", "tax",
+                                       "tad", "den", "rex", "pee"};
+    std::vector<std::vector<std::string>> expected{
+        {"red", "rex", "tex", "tax"},
+        {"red", "ted", "tad", "tax"},
+        {"red", "ted", "tex", "tax"}};
+    auto actual = Solution::findLadders(begin_word, end_word, word_list);
+    std::sort(actual.begin(), actual.end());
+    REQUIRE(expected == actual);
+  }
+  {
+    std::string begin_word = "aaaaa";
+    std::string end_word = "ggggg";
+    std::vector<std::string> word_list{
+        "aaaaa", "caaaa", "cbaaa", "daaaa", "dbaaa", "eaaaa", "ebaaa", "faaaa",
+        "fbaaa", "gaaaa", "gbaaa", "haaaa", "hbaaa", "iaaaa", "ibaaa", "jaaaa",
+        "jbaaa", "kaaaa", "kbaaa", "laaaa", "lbaaa", "maaaa", "mbaaa", "naaaa",
+        "nbaaa", "oaaaa", "obaaa", "paaaa", "pbaaa", "bbaaa", "bbcaa", "bbcba",
+        "bbdaa", "bbdba", "bbeaa", "bbeba", "bbfaa", "bbfba", "bbgaa", "bbgba",
+        "bbhaa", "bbhba", "bbiaa", "bbiba", "bbjaa", "bbjba", "bbkaa", "bbkba",
+        "bblaa", "bblba", "bbmaa", "bbmba", "bbnaa", "bbnba", "bboaa", "bboba",
+        "bbpaa", "bbpba", "bbbba", "abbba", "acbba", "dbbba", "dcbba", "ebbba",
+        "ecbba", "fbbba", "fcbba", "gbbba", "gcbba", "hbbba", "hcbba", "ibbba",
+        "icbba", "jbbba", "jcbba", "kbbba", "kcbba", "lbbba", "lcbba", "mbbba",
+        "mcbba", "nbbba", "ncbba", "obbba", "ocbba", "pbbba", "pcbba", "ccbba",
+        "ccaba", "ccaca", "ccdba", "ccdca", "cceba", "cceca", "ccfba", "ccfca",
+        "ccgba", "ccgca", "cchba", "cchca", "cciba", "ccica", "ccjba", "ccjca",
+        "cckba", "cckca", "cclba", "cclca", "ccmba", "ccmca", "ccnba", "ccnca",
+        "ccoba", "ccoca", "ccpba", "ccpca", "cccca", "accca", "adcca", "bccca",
+        "bdcca", "eccca", "edcca", "fccca", "fdcca", "gccca", "gdcca", "hccca",
+        "hdcca", "iccca", "idcca", "jccca", "jdcca", "kccca", "kdcca", "lccca",
+        "ldcca", "mccca", "mdcca", "nccca", "ndcca", "occca", "odcca", "pccca",
+        "pdcca", "ddcca", "ddaca", "ddada", "ddbca", "ddbda", "ddeca", "ddeda",
+        "ddfca", "ddfda", "ddgca", "ddgda", "ddhca", "ddhda", "ddica", "ddida",
+        "ddjca", "ddjda", "ddkca", "ddkda", "ddlca", "ddlda", "ddmca", "ddmda",
+        "ddnca", "ddnda", "ddoca", "ddoda", "ddpca", "ddpda", "dddda", "addda",
+        "aedda", "bddda", "bedda", "cddda", "cedda", "fddda", "fedda", "gddda",
+        "gedda", "hddda", "hedda", "iddda", "iedda", "jddda", "jedda", "kddda",
+        "kedda", "lddda", "ledda", "mddda", "medda", "nddda", "nedda", "oddda",
+        "oedda", "pddda", "pedda", "eedda", "eeada", "eeaea", "eebda", "eebea",
+        "eecda", "eecea", "eefda", "eefea", "eegda", "eegea", "eehda", "eehea",
+        "eeida", "eeiea", "eejda", "eejea", "eekda", "eekea", "eelda", "eelea",
+        "eemda", "eemea", "eenda", "eenea", "eeoda", "eeoea", "eepda", "eepea",
+        "eeeea", "ggggg", "agggg", "ahggg", "bgggg", "bhggg", "cgggg", "chggg",
+        "dgggg", "dhggg", "egggg", "ehggg", "fgggg", "fhggg", "igggg", "ihggg",
+        "jgggg", "jhggg", "kgggg", "khggg", "lgggg", "lhggg", "mgggg", "mhggg",
+        "ngggg", "nhggg", "ogggg", "ohggg", "pgggg", "phggg", "hhggg", "hhagg",
+        "hhahg", "hhbgg", "hhbhg", "hhcgg", "hhchg", "hhdgg", "hhdhg", "hhegg",
+        "hhehg", "hhfgg", "hhfhg", "hhigg", "hhihg", "hhjgg", "hhjhg", "hhkgg",
+        "hhkhg", "hhlgg", "hhlhg", "hhmgg", "hhmhg", "hhngg", "hhnhg", "hhogg",
+        "hhohg", "hhpgg", "hhphg", "hhhhg", "ahhhg", "aihhg", "bhhhg", "bihhg",
+        "chhhg", "cihhg", "dhhhg", "dihhg", "ehhhg", "eihhg", "fhhhg", "fihhg",
+        "ghhhg", "gihhg", "jhhhg", "jihhg", "khhhg", "kihhg", "lhhhg", "lihhg",
+        "mhhhg", "mihhg", "nhhhg", "nihhg", "ohhhg", "oihhg", "phhhg", "pihhg",
+        "iihhg", "iiahg", "iiaig", "iibhg", "iibig", "iichg", "iicig", "iidhg",
+        "iidig", "iiehg", "iieig", "iifhg", "iifig", "iighg", "iigig", "iijhg",
+        "iijig", "iikhg", "iikig", "iilhg", "iilig", "iimhg", "iimig", "iinhg",
+        "iinig", "iiohg", "iioig", "iiphg", "iipig", "iiiig", "aiiig", "ajiig",
+        "biiig", "bjiig", "ciiig", "cjiig", "diiig", "djiig", "eiiig", "ejiig",
+        "fiiig", "fjiig", "giiig", "gjiig", "hiiig", "hjiig", "kiiig", "kjiig",
+        "liiig", "ljiig", "miiig", "mjiig", "niiig", "njiig", "oiiig", "ojiig",
+        "piiig", "pjiig", "jjiig", "jjaig", "jjajg", "jjbig", "jjbjg", "jjcig",
+        "jjcjg", "jjdig", "jjdjg", "jjeig", "jjejg", "jjfig", "jjfjg", "jjgig",
+        "jjgjg", "jjhig", "jjhjg", "jjkig", "jjkjg", "jjlig", "jjljg", "jjmig",
+        "jjmjg", "jjnig", "jjnjg", "jjoig", "jjojg", "jjpig", "jjpjg", "jjjjg",
+        "ajjjg", "akjjg", "bjjjg", "bkjjg", "cjjjg", "ckjjg", "djjjg", "dkjjg",
+        "ejjjg", "ekjjg", "fjjjg", "fkjjg", "gjjjg", "gkjjg", "hjjjg", "hkjjg",
+        "ijjjg", "ikjjg", "ljjjg", "lkjjg", "mjjjg", "mkjjg", "njjjg", "nkjjg",
+        "ojjjg", "okjjg", "pjjjg", "pkjjg", "kkjjg", "kkajg", "kkakg", "kkbjg",
+        "kkbkg", "kkcjg", "kkckg", "kkdjg", "kkdkg", "kkejg", "kkekg", "kkfjg",
+        "kkfkg", "kkgjg", "kkgkg", "kkhjg", "kkhkg", "kkijg", "kkikg", "kkljg",
+        "kklkg", "kkmjg", "kkmkg", "kknjg", "kknkg", "kkojg", "kkokg", "kkpjg",
+        "kkpkg", "kkkkg", "ggggx", "gggxx", "ggxxx", "gxxxx", "xxxxx", "xxxxy",
+        "xxxyy", "xxyyy", "xyyyy", "yyyyy", "yyyyw", "yyyww", "yywww", "ywwww",
+        "wwwww", "wwvww", "wvvww", "vvvww", "vvvwz", "avvwz", "aavwz", "aaawz",
+        "aaaaz"};
+    std::vector<std::vector<std::string>> paths{
+        {"aaaaa", "aaaaz", "aaawz", "aavwz", "avvwz", "vvvwz", "vvvww",
+         "wvvww", "wwvww", "wwwww", "ywwww", "yywww", "yyyww", "yyyyw",
+         "yyyyy", "xyyyy", "xxyyy", "xxxyy", "xxxxy", "xxxxx", "gxxxx",
+         "ggxxx", "gggxx", "ggggx", "ggggg"}};
+    REQUIRE(paths == Solution::findLadders(begin_word, end_word, word_list));
+  }
+}
