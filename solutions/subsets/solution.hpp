eb4ea9a35015f85c351dad4b9d738d0d1e7901e4
Александр Морев <hikjik@yandex.ru>
2023-03-13 23:35:00 +0300

    Subsets

---
+++ solutions/subsets/solution.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<std::vector<int>> subsets(const std::vector<int> &nums) {
+    std::vector<std::vector<int>> subsets;
+    std::vector<int> subset;
+    enumerateSubsets(0, nums, &subset, &subsets);
+    return subsets;
+  }
+
+private:
+  static void enumerateSubsets(size_t i, const std::vector<int> &nums,
+                               std::vector<int> *subset,
+                               std::vector<std::vector<int>> *subsets) {
+    if (i == nums.size()) {
+      subsets->push_back(*subset);
+      return;
+    }
+
+    enumerateSubsets(i + 1, nums, subset, subsets);
+    subset->push_back(nums[i]);
+    enumerateSubsets(i + 1, nums, subset, subsets);
+    subset->pop_back();
+  }
+};
