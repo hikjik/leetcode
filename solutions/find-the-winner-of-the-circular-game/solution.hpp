be5bb1d4cf2b7c864e47421ab1d414c79d48b0be
Александр Морев <hikjik@yandex.ru>
2023-03-19 15:57:21 +0300

    Find the Winner of the Circular Game

---
+++ solutions/find-the-winner-of-the-circular-game/solution.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+class Solution {
+public:
+  static int findTheWinner(int n, int k) { return f(n, k) + 1; }
+
+private:
+  // f(n, k) - index of winner
+  // f(n, k) = (f(n - 1, k) + k) % n
+  // f(1, k) = 0
+  static int f(int n, int k) {
+    int winner_index = 0;
+    for (int i = 2; i <= n; ++i) {
+      winner_index = (winner_index + k) % i;
+    }
+    return winner_index;
+  }
+};
