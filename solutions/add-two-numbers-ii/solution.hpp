726778bd30fbf427bf66a87525b777d425e78af8
Александр Морев <hikjik@yandex.ru>
2023-03-20 01:56:49 +0300

    Add Two Numbers II

---
+++ solutions/add-two-numbers-ii/solution.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <list_node.h>
+
+#include <stack>
+
+class Solution {
+public:
+  static ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    auto stack1 = stackList(l1), stack2 = stackList(l2);
+    int carry = 0;
+    ListNode *head = nullptr;
+    while (!stack1.empty() || !stack2.empty() || carry) {
+      int val = carry;
+      if (!stack1.empty()) {
+        val += stack1.top();
+        stack1.pop();
+      }
+      if (!stack2.empty()) {
+        val += stack2.top();
+        stack2.pop();
+      }
+      carry = val / 10;
+      head = new ListNode(val % 10, head);
+    }
+    return head;
+  }
+
+private:
+  static std::stack<int> stackList(ListNode *node) {
+    std::stack<int> stack;
+    while (node) {
+      stack.push(node->val);
+      node = node->next;
+    }
+    return stack;
+  }
+};
