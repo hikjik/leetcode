a8daa7c7da8286d9f0ebeed9a05a19db83f9f2c4
Александр Морев <hikjik@yandex.ru>
2023-04-02 02:36:01 +0300

    LRU Cache

---
+++ solutions/lru-cache/solution.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <list>
+#include <unordered_map>
+#include <vector>
+
+class LRUCache {
+public:
+  LRUCache(int capacity) : capacity_(capacity) {}
+
+  int get(int key) {
+    if (auto it = nodes_.find(key); it == nodes_.end()) {
+      return -1;
+    } else {
+      items_.splice(items_.end(), items_, it->second);
+      return it->second->value;
+    }
+  }
+
+  void put(int key, int value) {
+    if (auto it = nodes_.find(key); it == nodes_.end()) {
+      nodes_[key] = items_.insert(items_.end(), {key, value});
+    } else {
+      it->second->value = value;
+      items_.splice(items_.end(), items_, it->second);
+    }
+
+    if (capacity_ < items_.size()) {
+      nodes_.erase(items_.begin()->key);
+      items_.pop_front();
+    }
+  }
+
+private:
+  struct Item {
+    int key;
+    int value;
+  };
+
+  std::list<Item> items_;
+  std::unordered_map<int, std::list<Item>::iterator> nodes_;
+  size_t capacity_;
+};
---
+++ solutions/lru-cache/test.cpp
@@ -0,0 +1,16 @@
+#include <catch.hpp>
+
+#include <solution.hpp>
+
+TEST_CASE("Simple") {
+  LRUCache cache(2);
+  cache.put(1, 1);
+  cache.put(2, 2);
+  REQUIRE(1 == cache.get(1));
+  cache.put(3, 3);
+  REQUIRE(-1 == cache.get(2));
+  cache.put(4, 4);
+  REQUIRE(-1 == cache.get(1));
+  REQUIRE(3 == cache.get(3));
+  REQUIRE(4 == cache.get(4));
+}
