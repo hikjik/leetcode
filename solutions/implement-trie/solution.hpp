7904eea748dc9eb985b2c2627485fa391da0ab06
Александр Морев <hikjik@yandex.ru>
2023-05-20 15:45:14 +0300

    Update Implement Trie

--- solutions/implement-trie/solution.hpp
+++ solutions/implement-trie/solution.hpp
@@ -1,50 +1,32 @@
 #pragma once
 
 #include <array>
+#include <memory>
 #include <string>
 
-class Trie {
-private:
-  struct TrieNode {
-    TrieNode() { std::fill(children.begin(), children.end(), nullptr); }
+struct TrieNode {
+  std::array<std::unique_ptr<TrieNode>, 26> children;
+  bool is_end = false;
+};
 
-    std::array<TrieNode *, 26> children;
-    bool is_end = false;
-  };
-
-  TrieNode *root_;
-
-  void FreeNode(TrieNode *node) {
-    if (!node) {
-      return;
-    }
-    for (auto child : node->children) {
-      FreeNode(child);
-    }
-    delete node;
-  }
-
+class Trie : private TrieNode {
 public:
-  Trie() : root_(new TrieNode()) {}
-
-  ~Trie() { FreeNode(root_); }
-
   void insert(std::string word) {
-    auto node = root_;
+    TrieNode *node = this;
     for (auto c : word) {
       const auto i = c - 'a';
       if (!node->children[i]) {
-        node->children[i] = new TrieNode();
+        node->children[i] = std::make_unique<TrieNode>();
       }
-      node = node->children[i];
+      node = node->children[i].get();
     }
     node->is_end = true;
   }
 
   TrieNode *getPrefix(std::string prefix) {
-    auto node = root_;
+    TrieNode *node = this;
     for (size_t i = 0; node && i < prefix.size(); ++i) {
-      node = node->children[prefix[i] - 'a'];
+      node = node->children[prefix[i] - 'a'].get();
     }
     return node;
   }
