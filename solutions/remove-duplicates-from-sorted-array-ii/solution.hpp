2d141c95cb8467151c438dda0362bb309fa3d48a
Александр Морев <hikjik@yandex.ru>
2023-05-21 15:32:24 +0300

    Remove Duplicates from Sorted Array II

--- solutions/remove-duplicates-from-sorted-array-ii/solution.hpp
+++ solutions/remove-duplicates-from-sorted-array-ii/solution.hpp
@@ -7,7 +7,7 @@ public:
   static int removeDuplicates(std::vector<int> &nums) {
     size_t size = 0;
     for (size_t i = 0; i < nums.size(); ++i) {
-      if (i < 1 || nums[i] != nums[size - 1]) {
+      if (i < 2 || nums[i] != nums[size - 2]) {
         nums[size++] = nums[i];
       }
     }
