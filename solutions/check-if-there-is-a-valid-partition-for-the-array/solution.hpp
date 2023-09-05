f3cc18d3261da8e2fae0f4d357926d9182e3d583
Морев Александр <hikjik@yandex.ru>
2023-09-05 20:19:05 +0300

    Check if There is a Valid Partition For The Array

---
+++ solutions/check-if-there-is-a-valid-partition-for-the-array/solution.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <array>
+#include <vector>
+
+class Solution {
+public:
+  bool validPartition(const std::vector<int> &nums) {
+    const int n = nums.size();
+    switch (n) {
+    case 1:
+      return false;
+    case 2:
+      return valid2(0, nums);
+    case 3:
+      return valid3(0, nums);
+    }
+
+    std::array<bool, 3> dp;
+    dp[0] = false;
+    dp[1] = valid2(n - 2, nums);
+    dp[2] = valid3(n - 3, nums);
+
+    for (int i = n - 4; i >= 0; --i) {
+      const auto d = valid2(i, nums) & dp[1] | valid3(i, nums) & dp[0];
+      dp[0] = dp[1], dp[1] = dp[2], dp[2] = d;
+    }
+
+    return dp[2];
+  }
+
+private:
+  bool valid2(int i, const std::vector<int> &nums) {
+    return nums[i] == nums[i + 1];
+  }
+
+  bool valid3(int i, const std::vector<int> &nums) {
+    return nums[i] == nums[i + 1] && nums[i] == nums[i + 2] ||
+           nums[i] == nums[i + 1] - 1 && nums[i] == nums[i + 2] - 2;
+  }
+};
---
+++ solutions/check-if-there-is-a-valid-partition-for-the-array/test.cpp
@@ -0,0 +1,51 @@
+#include <catch.hpp>
+
+#include <solution.hpp>
+
+TEST_CASE("Simple") {
+  {
+    std::vector<int> nums{4, 4, 4, 5, 6};
+    REQUIRE(Solution().validPartition(nums));
+  }
+  {
+    std::vector<int> nums{4, 4, 4, 10, 10, 1, 2, 3, 9, 9};
+    REQUIRE(Solution().validPartition(nums));
+  }
+  {
+    std::vector<int> nums{4, 4, 4, 10, 10, 1, 2, 3, 50, 9, 9};
+    REQUIRE_FALSE(Solution().validPartition(nums));
+  }
+  {
+    std::vector<int> nums{1, 1, 1, 2};
+    REQUIRE_FALSE(Solution().validPartition(nums));
+  }
+  {
+    std::vector<int> nums{1, 2};
+    REQUIRE_FALSE(Solution().validPartition(nums));
+  }
+  {
+    std::vector<int> nums{1};
+    REQUIRE_FALSE(Solution().validPartition(nums));
+  }
+  {
+    std::vector<int> nums{1, 1};
+    REQUIRE(Solution().validPartition(nums));
+  }
+  {
+    std::vector<int> nums{1, 2, 3};
+    REQUIRE(Solution().validPartition(nums));
+  }
+  {
+    std::vector<int> nums{1, 1, 1};
+    REQUIRE(Solution().validPartition(nums));
+  }
+  {
+    std::vector<int> nums{1, 1, 2};
+    REQUIRE_FALSE(Solution().validPartition(nums));
+  }
+  {
+    std::vector<int> nums{993335, 993336, 993337, 993338,
+                          993339, 993340, 993341};
+    REQUIRE_FALSE(Solution().validPartition(nums));
+  }
+}
