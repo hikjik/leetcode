80ce2ecb49c17624e806c36e8972d641bc01194c
Морев Александр <hikjik@yandex.ru>
2023-09-06 12:35:11 +0300

    Fix test

--- solutions/pow-x-n/test.cpp
+++ solutions/pow-x-n/test.cpp
@@ -6,19 +6,16 @@ TEST_CASE("Simple") {
   {
     double x = 2.00000;
     int n = 10;
-    double expected = 1024.00000;
-    REQUIRE(expected == Solution::myPow(x, n));
+    REQUIRE(1024.00000 == Approx(Solution::myPow(x, n)));
   }
   {
     double x = 2.10000;
     int n = 3;
-    double expected = 9.26100;
-    REQUIRE(expected == Solution::myPow(x, n));
+    REQUIRE(9.26100 == Approx(Solution::myPow(x, n)));
   }
   {
     double x = 2.00000;
     int n = -2;
-    double expected = 0.25000;
-    REQUIRE(expected == Solution::myPow(x, n));
+    REQUIRE(0.25000 == Approx(Solution::myPow(x, n)));
   }
 }
