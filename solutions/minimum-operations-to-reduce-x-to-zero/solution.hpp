b8078e940b0c399ea501b4b3a4bf028e9ad44c37
Морев Александр <hikjik@yandex.ru>
2023-09-20 08:50:12 +0300

    Minimum Operations to Reduce X to Zero

---
+++ solutions/minimum-operations-to-reduce-x-to-zero/solution.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+class Solution {
+public:
+  static int minOperations(const std::vector<int> &nums, int x) {
+    const auto sum = std::accumulate(nums.begin(), nums.end(), 0);
+    const auto length = maxSubArrayLen(nums, sum - x);
+    return length == -1 ? -1 : nums.size() - length;
+  }
+
+private:
+  static int maxSubArrayLen(const std::vector<int> &nums, int target_sum) {
+    const int n = nums.size();
+    int window_sum = 0;
+    int length = -1;
+    for (int l = 0, r = 0; r < n; ++r) {
+      window_sum += nums[r];
+      while (window_sum > target_sum && l <= r) {
+        window_sum -= nums[l++];
+      }
+      if (window_sum == target_sum) {
+        length = std::max(length, r - l + 1);
+      }
+    }
+    return length;
+  }
+};
