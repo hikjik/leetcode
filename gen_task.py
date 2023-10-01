a061b9a0813976db0dac97bbf63808d3eacbe8f4
Морев Александр <hikjik@yandex.ru>
2023-10-01 19:08:04 +0300

    Get description and code snippet

--- gen_task.py
+++ gen_task.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
+from dataclasses import dataclass
 from pathlib import Path
-from typing import List
+
+import requests
 
 CMAKE_LISTS_FILE = "CMakeLists.txt"
 CMAKE_LISTS_FILE_CONTENT = """\
@@ -11,9 +13,7 @@ SOLUTION_FILE = "solution.hpp"
 SOLUTION_FILE_CONTENT = """\
 #pragma once
 
-class Solution {
-public:
-};
+{snippet}
 """
 
 TEST_FILE = "test.cpp"
@@ -25,13 +25,82 @@ TEST_FILE_CONTENT = """\
 TEST_CASE("Simple") {}
 """
 
+DESCRIPTION_FILE = "description.md"
+DESCRIPTION_FILE_CONTENT = """\
+# [{frontend_id}. {title}](https://leetcode.com/problems/{title_slug}/)
 
-def update_cmake(new_task_name: str) -> None:
-    words = new_task_name.lower().strip().split()
-    new_task_name = "-".join(words)
+<p style="color:{color};">{difficulty}</p>
 
-    lines: List[str] = []
-    tasks: List[str] = [f"add_task({new_task_name})\n"]
+{content}
+"""
+
+DIFFICULTY_COLOR_MAP = {
+    "Easy": "green",
+    "Medium": "orange",
+    "Hard": "red",
+}
+
+
+@dataclass
+class Task:
+    id: str
+    frontend_id: str
+    title: str
+    title_slug: str
+    difficulty: str
+    content: str
+    code_snippets: dict[str, str]
+    meta_data: str
+
+
+def get_task_info(task_slug: str) -> Task:
+    url = "https://leetcode.com/graphql"
+
+    query = """
+    query questionData($titleSlug: String!) {
+        question(titleSlug: $titleSlug) {
+            questionId
+            questionFrontendId
+            title
+            titleSlug
+            difficulty
+            content
+            codeSnippets {
+                langSlug
+                code
+            }
+            metaData
+        }
+    }
+    """
+
+    data = {
+        "operationName": "questionData",
+        "variables": {"titleSlug": task_slug},
+        "query": query,
+    }
+
+    response = requests.post(url, json=data)
+    question = response.json()["data"]["question"]
+
+    snippets = {s["langSlug"]: s["code"] for s in question["codeSnippets"]}
+
+    return Task(
+        id=question["questionId"],
+        frontend_id=question["questionFrontendId"],
+        title=question["title"],
+        title_slug=question["titleSlug"],
+        difficulty=question["difficulty"],
+        content=question["content"],
+        code_snippets=snippets,
+        meta_data=question["metaData"],
+    )
+
+
+def update_cmake(task_slug: str) -> None:
+    lines: list[str] = []
+    tasks: list[str] = [f"add_task({task_slug})\n"]
+
     with open(CMAKE_LISTS_FILE, "r+") as file:
         for line in file:
             if line.startswith("add_task"):
@@ -47,26 +116,57 @@ def update_cmake(new_task_name: str) -> None:
             file.write(task)
 
 
-def create_task_folder(new_task_name: str) -> None:
-    words = new_task_name.lower().strip().split()
+def create_task_folder(task_slug: str) -> None:
+    task = get_task_info(task_slug)
 
-    task_path = Path("solutions") / Path("-".join(words))
+    task_path = Path("solutions") / Path(task_slug)
     task_path.mkdir()
 
     with open(task_path / SOLUTION_FILE, "w") as file:
-        file.write(SOLUTION_FILE_CONTENT)
+        file.write(
+            SOLUTION_FILE_CONTENT.format(
+                snippet=task.code_snippets["cpp"],
+            )
+        )
 
     with open(task_path / TEST_FILE, "w") as file:
         file.write(TEST_FILE_CONTENT)
 
     with open(task_path / CMAKE_LISTS_FILE, "w") as file:
-        file.write(CMAKE_LISTS_FILE_CONTENT.format(task_name="_".join(words)))
+        file.write(
+            CMAKE_LISTS_FILE_CONTENT.format(
+                task_name=task_slug.replace("-", "_"),
+            )
+        )
+
+    with open(task_path / DESCRIPTION_FILE, "w") as file:
+        file.write(
+            DESCRIPTION_FILE_CONTENT.format(
+                frontend_id=task.frontend_id,
+                title=task.title,
+                title_slug=task.title_slug,
+                color=DIFFICULTY_COLOR_MAP[task.difficulty],
+                difficulty=task.difficulty,
+                content=task.content,
+            )
+        )
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("task_name", type=str)
+    parser.add_argument(
+        "--slug",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--lang",
+        type=str,
+        choices=["cpp"],
+        default="cpp",
+        required=False,
+    )
     args = parser.parse_args()
 
-    create_task_folder(args.task_name)
-    update_cmake(args.task_name)
+    create_task_folder(args.slug)
+    # update_cmake(args.slug)
