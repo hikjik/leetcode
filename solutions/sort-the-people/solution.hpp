06802f202b381e421a6fe6f84dee259ced874081
Морев Александр <hikjik@yandex.ru>
2023-10-18 00:49:15 +0300

    Sort the People

---
+++ solutions/sort-the-people/solution.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<std::string>
+  sortPeople(const std::vector<std::string> &names,
+             const std::vector<int> &heights) {
+    std::vector<std::pair<int, std::string>> people;
+    for (size_t i = 0; i < names.size(); ++i) {
+      people.push_back({heights[i], names[i]});
+    }
+
+    std::sort(people.begin(), people.end(), std::greater<>());
+
+    std::vector<std::string> ans;
+    for (const auto &[_, name] : people) {
+      ans.push_back(name);
+    }
+    return ans;
+  }
+};
