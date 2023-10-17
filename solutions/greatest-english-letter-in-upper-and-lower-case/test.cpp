af3397a6a74817e330f17b87a76c44e4e15e7b35
Морев Александр <hikjik@yandex.ru>
2023-10-17 03:10:01 +0300

    Format tests

--- solutions/greatest-english-letter-in-upper-and-lower-case/test.cpp
+++ solutions/greatest-english-letter-in-upper-and-lower-case/test.cpp
@@ -4,14 +4,26 @@
 
 TEST_CASE("Simple") {
   struct TestCase {
-    std::string s;std::string expected;
+    std::string s;
+    std::string expected;
   };
 
   std::vector<TestCase> test_cases{
-      {.s="lEeTcOdE",.expected="E",},{.s="arRAzFif",.expected="R",},{.s="AbCdEfGhIjK",.expected="",},
+      {
+          .s = "lEeTcOdE",
+          .expected = "E",
+      },
+      {
+          .s = "arRAzFif",
+          .expected = "R",
+      },
+      {
+          .s = "AbCdEfGhIjK",
+          .expected = "",
+      },
   };
 
-  for (const auto &[s,expected] : test_cases) {
+  for (const auto &[s, expected] : test_cases) {
     const auto actual = Solution::greatestLetter(s);
     REQUIRE(expected == actual);
   }
