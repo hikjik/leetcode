b9db772ec7473d332d102a05e20f598e76a6dc6e
Морев Александр <hikjik@yandex.ru>
2023-10-18 02:46:36 +0300

    Fix Sort People

--- solutions/sort-the-people/test.cpp
+++ solutions/sort-the-people/test.cpp
@@ -4,14 +4,25 @@
 
 TEST_CASE("Simple") {
   struct TestCase {
-    std::vector<std::string> names;std::vector<int> heights;std::vector<std::string> expected;
+    std::vector<std::string> names;
+    std::vector<int> heights;
+    std::vector<std::string> expected;
   };
 
   std::vector<TestCase> test_cases{
-      {.names{"Mary","John","Emma"},.heights{180,165,170},.expected{"Mary","Emma","John"},},{.names{"Alice","Bob","Bob"},.heights{155,185,150},.expected{"Bob","Alice","Bob"},},
+      {
+          .names{"Mary", "John", "Emma"},
+          .heights{180, 165, 170},
+          .expected{"Mary", "Emma", "John"},
+      },
+      {
+          .names{"Alice", "Bob", "Bob"},
+          .heights{155, 185, 150},
+          .expected{"Bob", "Alice", "Bob"},
+      },
   };
 
-  for (const auto &[names,heights,expected] : test_cases) {
+  for (const auto &[names, heights, expected] : test_cases) {
     const auto actual = Solution::sortPeople(names, heights);
     REQUIRE(expected == actual);
   }
