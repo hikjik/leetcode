52b0c21df8f0b0ff6cfca7d87e311a9e31e8733a
Морев Александр <hikjik@yandex.ru>
2023-10-18 08:12:20 +0300

    Categorize Box According to Criteria

---
+++ solutions/categorize-box-according-to-criteria/solution.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  static std::string categorizeBox(int length, int width, int height,
+                                   int mass) {
+    const auto volume = 1LL * width * height * length;
+    const auto isBulky =
+        length >= 1e4 || width >= 1e4 || height >= 1e4 || volume >= 1e9;
+    const auto isHeavy = mass >= 100;
+
+    if (isBulky && isHeavy) {
+      return "Both";
+    }
+    if (isBulky) {
+      return "Bulky";
+    }
+    if (isHeavy) {
+      return "Heavy";
+    }
+    return "Neither";
+  }
+};
