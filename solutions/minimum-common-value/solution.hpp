5e859b3c17e58a1a2c0a12c56e3dfaba070a5ec5
Морев Александр <hikjik@yandex.ru>
2023-10-18 07:58:50 +0300

    Minimum Common Value

---
+++ solutions/minimum-common-value/solution.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static int getCommon(const std::vector<int> &nums1,
+                       const std::vector<int> &nums2) {
+
+    for (size_t i = 0, j = 0; i < nums1.size() && j < nums2.size();) {
+      if (nums1[i] == nums2[j]) {
+        return nums1[i];
+      }
+      nums1[i] < nums2[j] ? ++i : ++j;
+    }
+    return -1;
+  }
+};
