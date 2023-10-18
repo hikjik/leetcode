c96cd60852093e1ecd2625e59f62f6571825cf84
Морев Александр <hikjik@yandex.ru>
2023-10-19 02:02:00 +0300

    Find the Distinct Difference Array

---
+++ solutions/find-the-distinct-difference-array/solution.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int>
+  distinctDifferenceArray(const std::vector<int> &nums) {
+    std::unordered_map<int, int> left, right;
+    for (auto num : nums) {
+      ++right[num];
+    }
+
+    std::vector<int> ans;
+    int l = 0, r = right.size();
+    for (auto num : nums) {
+      if (!left[num]++) {
+        ++l;
+      }
+      if (!--right[num]) {
+        --r;
+      }
+      ans.push_back(l - r);
+    }
+    return ans;
+  }
+};
