c12a945e46f0045298f9480f4dea64ec921a14b7
Морев Александр <hikjik@yandex.ru>
2023-10-19 00:22:10 +0300

    Distribute Money to Maximum Children

---
+++ solutions/distribute-money-to-maximum-children/solution.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <algorithm>
+
+class Solution {
+public:
+  static int distMoney(int money, int children) {
+    if (money < children) {
+      return -1;
+    }
+
+    const auto q = (money - children) / 7;
+    const auto r = (money - children) % 7;
+
+    if (q == children && r == 0) {
+      return q;
+    }
+    if (q == children - 1 && r == 3) {
+      return q - 1;
+    }
+    return std::min(q, children - 1);
+  }
+};
