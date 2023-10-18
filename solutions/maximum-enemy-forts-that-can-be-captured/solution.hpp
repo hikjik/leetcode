d16ba5f06296f69a854fc503b1f63b753ce03c5c
Морев Александр <hikjik@yandex.ru>
2023-10-18 09:52:28 +0300

    Maximum Enemy Forts That Can Be Captured

---
+++ solutions/maximum-enemy-forts-that-can-be-captured/solution.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static int captureForts(const std::vector<int> &forts) {
+    int ans = 0;
+    for (int i = 0, j = 0; j < std::ssize(forts); ++j) {
+      if (forts[j]) {
+        if (forts[i] == -forts[j]) {
+          ans = std::max(ans, j - i - 1);
+        }
+        i = j;
+      }
+    }
+    return ans;
+  }
+};
