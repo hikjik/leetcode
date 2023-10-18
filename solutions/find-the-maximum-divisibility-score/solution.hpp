eef47bae32813cbc4c03493c3bcd3d33b62efb30
Морев Александр <hikjik@yandex.ru>
2023-10-19 01:12:37 +0300

    Find the Maximum Divisibility Score

---
+++ solutions/find-the-maximum-divisibility-score/solution.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+  static int maxDivScore(const std::vector<int> &nums,
+                         const std::vector<int> &divisors) {
+    int max_score = -1, divisor = -1;
+    for (auto d : divisors) {
+      const auto score = std::count_if(nums.begin(), nums.end(),
+                                       [d](int n) { return n % d == 0; });
+      if (score > max_score) {
+        max_score = score;
+        divisor = d;
+      } else if (score == max_score) {
+        divisor = std::min(divisor, d);
+      }
+    }
+    return divisor;
+  }
+};
