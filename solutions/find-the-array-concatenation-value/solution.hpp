2e1677f0577077a6a0a542b68165f217066c101b
Морев Александр <hikjik@yandex.ru>
2023-10-18 20:50:47 +0300

    Find the Array Concatenation Value

---
+++ solutions/find-the-array-concatenation-value/solution.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cmath>
+#include <vector>
+
+class Solution {
+public:
+  static long long findTheArrayConcVal(const std::vector<int> &nums) {
+    const int n = nums.size();
+    auto value = 0LL;
+    for (int i = 0; i < n / 2; ++i) {
+      const int digits = std::log10(nums[n - i - 1]) + 1;
+      value += nums[i] * std::pow(10, digits) + nums[n - i - 1];
+    }
+    if (n & 1) {
+      value += nums[n / 2];
+    }
+    return value;
+  }
+};
