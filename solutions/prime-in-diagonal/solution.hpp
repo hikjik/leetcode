855f63bf848267bace36aa4fb7a3b9c7fe192243
Морев Александр <hikjik@yandex.ru>
2023-10-19 00:33:46 +0300

    Prime In Diagonal

---
+++ solutions/prime-in-diagonal/solution.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static int diagonalPrime(const std::vector<std::vector<int>> &nums) {
+    const int n = nums.size();
+
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      if (isPrime(nums[i][i])) {
+        ans = std::max(ans, nums[i][i]);
+      }
+      if (isPrime(nums[i][n - i - 1])) {
+        ans = std::max(ans, nums[i][n - i - 1]);
+      }
+    }
+    return ans;
+  }
+
+private:
+  static bool isPrime(int n) {
+    for (int i = 2; i * i <= n; ++i) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return n != 1;
+  }
+};
