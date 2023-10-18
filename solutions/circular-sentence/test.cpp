51c17af3503e46e98c5463011324a0dfd1bbaf29
Морев Александр <hikjik@yandex.ru>
2023-10-18 04:58:35 +0300

    Circular Sentence

---
+++ solutions/circular-sentence/test.cpp
@@ -0,0 +1,18 @@
+#include <catch.hpp>
+
+#include <solution.hpp>
+
+TEST_CASE("Simple") {
+  struct TestCase {
+    std::string sentence;bool expected;
+  };
+
+  std::vector<TestCase> test_cases{
+      {.sentence="leetcode exercises sound delightful",.expected=true,},{.sentence="eetcode",.expected=true,},{.sentence="Leetcode is cool",.expected=false,},
+  };
+
+  for (const auto &[sentence,expected] : test_cases) {
+    const auto actual = Solution::isCircularSentence(sentence);
+    REQUIRE(expected == actual);
+  }
+}
