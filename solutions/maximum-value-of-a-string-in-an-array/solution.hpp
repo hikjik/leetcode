1fdfa85f2602a4c03d52c62c509eca07aa7c9ada
Морев Александр <hikjik@yandex.ru>
2023-10-18 04:48:12 +0300

    Maximum Value of a String in an Array

---
+++ solutions/maximum-value-of-a-string-in-an-array/solution.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+  static int maximumValue(const std::vector<std::string> &strs) {
+    int max_value = 0;
+    for (const auto &str : strs) {
+      max_value = std::max(max_value, computeValue(str));
+    }
+    return max_value;
+  }
+
+private:
+  static int computeValue(const std::string &str) {
+    int value = 0;
+    for (auto c : str) {
+      if (std::isdigit(c)) {
+        value = value * 10 + c - '0';
+      } else {
+        return str.size();
+      }
+    }
+    return value;
+  }
+};
