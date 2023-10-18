1c175819c2cfd3618f5b257a28ec4ed5a603282d
Морев Александр <hikjik@yandex.ru>
2023-10-18 08:58:54 +0300

    Count Pairs Of Similar Strings

---
+++ solutions/count-pairs-of-similar-strings/solution.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+  static int similarPairs(const std::vector<std::string> &words) {
+    std::unordered_map<std::string, int> counter;
+    int cnt = 0;
+    for (const auto &word : words) {
+      cnt += counter[normalize(word)]++;
+    }
+    return cnt;
+  }
+
+private:
+  static std::string normalize(std::string str) {
+    std::sort(str.begin(), str.end());
+    return {str.begin(), std::unique(str.begin(), str.end())};
+  }
+};
