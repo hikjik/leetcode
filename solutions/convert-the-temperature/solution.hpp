5b8e776169b05fc2460c24000c26d601298fb068
Морев Александр <hikjik@yandex.ru>
2023-10-18 05:38:23 +0300

    Convert the Temperature

---
+++ solutions/convert-the-temperature/solution.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<double> convertTemperature(double celsius) {
+    const auto kelvin = celsius + 273.15;
+    const auto fahrenheit = celsius * 1.80 + 32.00;
+    return {kelvin, fahrenheit};
+  }
+};
