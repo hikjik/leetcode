b631b49e23512b279288f691b4237edeb562d99a
Морев Александр <hikjik@yandex.ru>
2023-10-18 05:05:30 +0300

    Find the Pivot Integer

---
+++ solutions/find-the-pivot-integer/solution.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cmath>
+
+class Solution {
+public:
+  static int pivotInteger(int n) {
+    const int sum = (n + 1) * n / 2;
+    const int sqrt = std::sqrt(sum);
+    return sqrt * sqrt == sum ? sqrt : -1;
+  }
+};
