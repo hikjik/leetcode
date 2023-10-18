2996e02fbe09f4e602c99d652a1519266ec0e0d8
Морев Александр <hikjik@yandex.ru>
2023-10-18 04:58:35 +0300

    Circular Sentence

---
+++ solutions/circular-sentence/solution.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  static bool isCircularSentence(std::string sentence) {
+    for (size_t i = 0; i < sentence.size(); ++i) {
+      if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1]) {
+        return false;
+      }
+    }
+    return sentence.front() == sentence.back();
+  }
+};
