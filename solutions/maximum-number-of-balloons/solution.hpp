a0ac78a6e5928d8e45d2f889d6514dc5fd6c7d00
Морев Александр <hikjik@yandex.ru>
2023-10-19 14:29:53 +0300

    Maximum Number of Balloons

---
+++ solutions/maximum-number-of-balloons/solution.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <climits>
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+  static int maxNumberOfBalloons(std::string text) {
+    auto map = buildMap(text);
+    int ans = INT_MAX;
+    for (const auto &[c, f] : buildMap("balloon")) {
+      ans = std::min(ans, map[c] / f);
+    }
+    return ans;
+  }
+
+  static std::unordered_map<char, int> buildMap(std::string str) {
+    std::unordered_map<char, int> map;
+    for (auto c : str) {
+      ++map[c];
+    }
+    return map;
+  }
+};
