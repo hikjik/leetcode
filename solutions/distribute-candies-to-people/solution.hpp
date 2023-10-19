acfb0deee0b09416f3b8ef8b39033c92035f0693
Морев Александр <hikjik@yandex.ru>
2023-10-19 13:42:39 +0300

    Distribute Candies to People

---
+++ solutions/distribute-candies-to-people/solution.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int> distributeCandies(int candies, int num_people) {
+    std::vector<int> ans(num_people);
+    for (int i = 0; candies > 0; ++i, candies -= i) {
+      ans[i % num_people] += std::min(i + 1, candies);
+    }
+    return ans;
+  }
+};
