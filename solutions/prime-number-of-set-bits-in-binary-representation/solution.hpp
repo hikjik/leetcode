1621ffea8fa5d6080624c1fddd4f0cf9cfd72345
Морев Александр <hikjik@yandex.ru>
2023-10-19 16:00:17 +0300

    Prime Number of Set Bits in Binary Representation

---
+++ solutions/prime-number-of-set-bits-in-binary-representation/solution.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <unordered_set>
+
+class Solution {
+public:
+  static int countPrimeSetBits(int left, int right) {
+    std::unordered_set<int> primes{2, 3, 5, 7, 11, 13, 17, 19};
+    int cnt = 0;
+    for (int i = left; i <= right; ++i) {
+      if (primes.contains(__builtin_popcount(i))) {
+        ++cnt;
+      }
+    }
+    return cnt;
+  }
+};
