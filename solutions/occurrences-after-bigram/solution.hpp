0cb85ac5dcd021d9afc9a97799eca66e534b9361
Морев Александр <hikjik@yandex.ru>
2023-10-19 04:17:51 +0300

    Occurrences After Bigram

---
+++ solutions/occurrences-after-bigram/solution.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<std::string>
+  findOcurrences(std::string text, std::string first, std::string second) {
+    std::stringstream stream(text);
+    std::vector<std::string> words;
+    for (std::string p1, p2, word; stream >> word; p1 = p2, p2 = word) {
+      if (p1 == first && p2 == second) {
+        words.push_back(word);
+      }
+    }
+    return words;
+  }
+};
