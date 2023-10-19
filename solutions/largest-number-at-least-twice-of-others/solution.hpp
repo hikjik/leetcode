d7b7895c0925076525348141b48f6131706369c7
Морев Александр <hikjik@yandex.ru>
2023-10-19 12:41:59 +0300

    Largest Number At Least Twice of Others

---
+++ solutions/largest-number-at-least-twice-of-others/solution.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+  static int dominantIndex(std::vector<int> nums) {
+    const auto idx = std::max_element(nums.begin(), nums.end()) - nums.begin();
+    std::partial_sort(nums.begin(), nums.begin() + 2, nums.end(),
+                      std::greater<int>());
+    return nums[0] >= nums[1] * 2 ? idx : -1;
+  }
+};
