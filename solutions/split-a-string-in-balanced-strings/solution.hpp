b4384a0d00061e86f962093ca38ef8fce69b154c
Морев Александр <hikjik@yandex.ru>
2023-10-20 01:25:18 +0300

    Split a String in Balanced Strings

---
+++ solutions/split-a-string-in-balanced-strings/solution.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  static int balancedStringSplit(std::string s) {
+    int ans = 0, cnt = 0;
+    for (auto c : s) {
+      cnt += c == 'R' ? 1 : -1;
+      ans += !cnt;
+    }
+    return ans;
+  }
+};
