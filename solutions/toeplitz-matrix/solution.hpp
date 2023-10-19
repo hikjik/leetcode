604af3d81e617167075c65264e4ed471038b7341
Морев Александр <hikjik@yandex.ru>
2023-10-19 21:32:27 +0300

    Toeplitz Matrix

---
+++ solutions/toeplitz-matrix/solution.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static bool isToeplitzMatrix(const std::vector<std::vector<int>> &matrix) {
+    const int m = matrix.size(), n = matrix.back().size();
+    for (int i = 1; i < m; ++i) {
+      for (int j = 1; j < n; ++j) {
+        if (matrix[i][j] != matrix[i - 1][j - 1]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
