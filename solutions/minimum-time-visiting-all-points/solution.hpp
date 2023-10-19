0f1510fa36dc257bc42f9d674d0e7ad4d779ff8f
Морев Александр <hikjik@yandex.ru>
2023-10-20 01:53:55 +0300

    Minimum Time Visiting All Points

---
+++ solutions/minimum-time-visiting-all-points/solution.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cmath>
+#include <vector>
+
+class Solution {
+public:
+  static int
+  minTimeToVisitAllPoints(const std::vector<std::vector<int>> &points) {
+    int ans = 0;
+    for (int i = 1; i < std::ssize(points); ++i) {
+      ans += std::max(std::abs(points[i][0] - points[i - 1][0]),
+                      std::abs(points[i][1] - points[i - 1][1]));
+    }
+    return ans;
+  }
+};
