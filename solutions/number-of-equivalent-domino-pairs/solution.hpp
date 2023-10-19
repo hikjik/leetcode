8167196f3d32c6285cdc082d0a3526cd86d4b461
Морев Александр <hikjik@yandex.ru>
2023-10-19 12:08:41 +0300

    Number of Equivalent Domino Pairs

---
+++ solutions/number-of-equivalent-domino-pairs/solution.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+  static int
+  numEquivDominoPairs(const std::vector<std::vector<int>> &dominoes) {
+    std::unordered_map<int, int> map;
+    int ans = 0;
+    for (const auto &domino : dominoes) {
+      ans += map[1 << domino[0] | 1 << domino[1]]++;
+    }
+    return ans;
+  }
+};
