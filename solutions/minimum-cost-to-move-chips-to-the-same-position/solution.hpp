9a3a66d6495f7e9dfde10ccf6ec467845ca221c4
Морев Александр <hikjik@yandex.ru>
2023-10-20 01:33:57 +0300

    Minimum Cost to Move Chips to The Same Position

---
+++ solutions/minimum-cost-to-move-chips-to-the-same-position/solution.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static int minCostToMoveChips(const std::vector<int> &position) {
+    const int even = std::count_if(position.begin(), position.end(),
+                                   [](int a) { return a % 2 == 0; });
+    const int odd = position.size() - even;
+    return std::min(even, odd);
+  }
+};
