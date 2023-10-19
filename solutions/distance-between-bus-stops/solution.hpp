adf3253e68277a1367772bed8fe3840fe376609f
Морев Александр <hikjik@yandex.ru>
2023-10-19 22:14:12 +0300

    Distance Between Bus Stops

---
+++ solutions/distance-between-bus-stops/solution.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static int distanceBetweenBusStops(const std::vector<int> &distance,
+                                     int start, int destination) {
+    if (start > destination) {
+      std::swap(start, destination);
+    }
+
+    int clockwise = 0, counterclockwise = 0;
+    for (int i = 0; i < std::ssize(distance); ++i) {
+      if (i >= start && i < destination) {
+        clockwise += distance[i];
+      } else {
+        counterclockwise += distance[i];
+      }
+    }
+    return std::min(clockwise, counterclockwise);
+  }
+};
