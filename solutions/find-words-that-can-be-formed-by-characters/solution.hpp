b6e6034f3d9ab1ad7850f1e15db99dd59225190e
Морев Александр <hikjik@yandex.ru>
2023-10-19 15:07:29 +0300

    Find Words That Can Be Formed by Characters

---
+++ solutions/find-words-that-can-be-formed-by-characters/solution.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+  static int countCharacters(std::vector<std::string> words,
+                             std::string chars) {
+    std::sort(chars.begin(), chars.end());
+    int ans = 0;
+    for (auto word : words) {
+      std::sort(word.begin(), word.end());
+      if (std::includes(chars.begin(), chars.end(), word.begin(), word.end())) {
+        ans += word.size();
+      }
+    }
+    return ans;
+  }
+};
