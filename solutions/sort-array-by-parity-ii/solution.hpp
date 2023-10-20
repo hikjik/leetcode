96ee0e1149f5471fd5f0750fdebf1446f2f58389
Морев Александр <hikjik@yandex.ru>
2023-10-20 23:41:25 +0300

    Sort Array By Parity II

---
+++ solutions/sort-array-by-parity-ii/solution.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int> sortArrayByParityII(const std::vector<int> &nums) {
+    std::vector<int> ans(nums.size());
+    for (int even = -2, odd = -1; auto a : nums) {
+      ans[a & 1 ? odd += 2 : even += 2] = a;
+    }
+    return ans;
+  }
+};
