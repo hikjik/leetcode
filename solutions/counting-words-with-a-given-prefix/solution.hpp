49daf2a322bde9b250d17b26f03bc239c7aa1ef9
Морев Александр <hikjik@yandex.ru>
2023-10-20 15:57:24 +0300

    Counting Words With a Given Prefix

---
+++ solutions/counting-words-with-a-given-prefix/solution.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+  static int prefixCount(const std::vector<std::string> &words,
+                         std::string pref) {
+    return count_if(words.begin(), words.end(), [&pref](const auto &word) {
+      return word.starts_with(pref);
+    });
+  }
+};
