df89474f0c377e3a54d7beaa9fdeaae14b4a4e63
Морев Александр <hikjik@yandex.ru>
2023-10-21 02:01:01 +0300

    Most Common Word

---
+++ solutions/most-common-word/solution.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+public:
+  static std::string mostCommonWord(std::string paragraph,
+                                    const std::vector<std::string> &banned) {
+    for (auto &c : paragraph) {
+      c = std::isalpha(c) ? std::tolower(c) : ' ';
+    }
+
+    std::unordered_set<std::string> banned_set(banned.begin(), banned.end());
+    std::unordered_map<std::string, int> counter;
+    std::stringstream stream(paragraph);
+    for (std::string word; stream >> word;) {
+      if (!banned_set.contains(word)) {
+        ++counter[word];
+      }
+    }
+
+    return std::max_element(counter.begin(), counter.end(),
+                            [](const auto &lhs, const auto &rhs) {
+                              return lhs.second < rhs.second;
+                            })
+        ->first;
+  }
+};
