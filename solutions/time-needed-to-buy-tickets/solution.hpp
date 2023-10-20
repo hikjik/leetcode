0ae9cec0512d4d5f1e2433096ba4767a9834e5df
Морев Александр <hikjik@yandex.ru>
2023-10-20 10:41:56 +0300

    Time Needed to Buy Tickets

---
+++ solutions/time-needed-to-buy-tickets/solution.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static int timeRequiredToBuy(const std::vector<int> &tickets, int k) {
+    int time = 0;
+    for (int i = 0; i < std::ssize(tickets); ++i) {
+      time += std::min(tickets[i], tickets[k] - (i > k));
+    }
+    return time;
+  }
+};
