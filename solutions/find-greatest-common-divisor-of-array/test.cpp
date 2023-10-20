153eefdbf4c4e2e93789ac473febdafe64cc4543
Морев Александр <hikjik@yandex.ru>
2023-10-20 09:30:05 +0300

    Find Greatest Common Divisor of Array

---
+++ solutions/find-greatest-common-divisor-of-array/test.cpp
@@ -0,0 +1,18 @@
+#include <catch.hpp>
+
+#include <solution.hpp>
+
+TEST_CASE("Simple") {
+  struct TestCase {
+    std::vector<int> nums;int expected;
+  };
+
+  std::vector<TestCase> test_cases{
+      {.nums{2,5,6,9,10},.expected=2,},{.nums{7,5,6,8,3},.expected=1,},{.nums{3,3},.expected=3,},
+  };
+
+  for (const auto &[nums,expected] : test_cases) {
+    const auto actual = Solution::findGCD(nums);
+    REQUIRE(expected == actual);
+  }
+}
