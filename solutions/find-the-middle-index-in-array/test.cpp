9a3e59423e4188395b99da0fb26d62c60a75a6dd
Морев Александр <hikjik@yandex.ru>
2023-10-20 10:25:04 +0300

    Clang format test.cpp on create

--- solutions/find-the-middle-index-in-array/test.cpp
+++ solutions/find-the-middle-index-in-array/test.cpp
@@ -4,14 +4,26 @@
 
 TEST_CASE("Simple") {
   struct TestCase {
-    std::vector<int> nums;int expected;
+    std::vector<int> nums;
+    int expected;
   };
 
   std::vector<TestCase> test_cases{
-      {.nums{2,3,-1,8,4},.expected=3,},{.nums{1,-1,4},.expected=2,},{.nums{2,5},.expected=-1,},
+      {
+          .nums{2, 3, -1, 8, 4},
+          .expected = 3,
+      },
+      {
+          .nums{1, -1, 4},
+          .expected = 2,
+      },
+      {
+          .nums{2, 5},
+          .expected = -1,
+      },
   };
 
-  for (const auto &[nums,expected] : test_cases) {
+  for (const auto &[nums, expected] : test_cases) {
     const auto actual = Solution::findMiddleIndex(nums);
     REQUIRE(expected == actual);
   }
