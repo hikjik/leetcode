751c98686bce20e5c2108d251df3bb4f9d6fabf7
Морев Александр <hikjik@yandex.ru>
2023-10-20 10:13:21 +0300

    Count Special Quadruplets

---
+++ solutions/count-special-quadruplets/solution.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+  static int countQuadruplets(const std::vector<int> &nums) {
+    std::unordered_map<int, int> map;
+    int cnt = 0;
+    for (int b = 1; b < std::ssize(nums); ++b) {
+      for (int a = 0; a < b; ++a) {
+        ++map[nums[a] + nums[b]];
+      }
+      for (int c = b + 1, d = c + 1; d < std::ssize(nums); ++d) {
+        cnt += map[nums[d] - nums[c]];
+      }
+    }
+    return cnt;
+  }
+};
