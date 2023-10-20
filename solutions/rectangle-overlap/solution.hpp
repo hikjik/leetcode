d36b79ad70b46cf61b9bc112f735e535f48347ac
Морев Александр <hikjik@yandex.ru>
2023-10-20 22:35:42 +0300

    Rectangle Overlap

---
+++ solutions/rectangle-overlap/solution.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static bool isRectangleOverlap(const std::vector<int> &rec1,
+                                 const std::vector<int> &rec2) {
+    return isSegmentOverlap({rec1[0], rec1[2]}, {rec2[0], rec2[2]}) &&
+           isSegmentOverlap({rec1[1], rec1[3]}, {rec2[1], rec2[3]});
+  }
+
+  static bool isSegmentOverlap(const std::pair<int, int> &segment1,
+                               const std::pair<int, int> &segment2) {
+    return segment1.first < segment2.second && segment2.first < segment1.second;
+  }
+};
