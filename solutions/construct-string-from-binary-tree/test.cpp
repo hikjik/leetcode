dab8cf8c7bef595469f0f65d0ff538aa07f3d8aa
Морев Александр <hikjik@yandex.ru>
2023-10-20 09:35:39 +0300

    Format tests

--- solutions/construct-string-from-binary-tree/test.cpp
+++ solutions/construct-string-from-binary-tree/test.cpp
@@ -6,14 +6,22 @@
 
 TEST_CASE("Simple") {
   struct TestCase {
-    Tree root;std::string expected;
+    Tree root;
+    std::string expected;
   };
 
   std::vector<TestCase> test_cases{
-      {.root={1,2,3,4},.expected="1(2(4))(3)",},{.root={1,2,3,std::nullopt,4},.expected="1(2()(4))(3)",},
+      {
+          .root = {1, 2, 3, 4},
+          .expected = "1(2(4))(3)",
+      },
+      {
+          .root = {1, 2, 3, std::nullopt, 4},
+          .expected = "1(2()(4))(3)",
+      },
   };
 
-  for (const auto &[root,expected] : test_cases) {
+  for (const auto &[root, expected] : test_cases) {
     const auto actual = Solution::tree2str(root);
     REQUIRE(expected == actual);
   }
