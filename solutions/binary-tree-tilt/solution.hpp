92bcf65d17fa6e89ce99c1754d56955398038187
Морев Александр <hikjik@yandex.ru>
2023-10-21 16:20:32 +0300

    Binary Tree Tilt

---
+++ solutions/binary-tree-tilt/solution.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <tree_node.h>
+
+class Solution {
+public:
+  static int findTilt(TreeNode *root) {
+    int sum = 0;
+    findTilt(root, &sum);
+    return sum;
+  }
+
+private:
+  static int findTilt(TreeNode *root, int *sum) {
+    if (!root) {
+      return 0;
+    }
+    const auto left = findTilt(root->left, sum);
+    const auto right = findTilt(root->right, sum);
+    *sum += std::abs(left - right);
+    return root->val + left + right;
+  }
+};
