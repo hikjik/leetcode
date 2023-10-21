bf9a4e19fad18be399a227ffb4d58d197281bd62
Морев Александр <hikjik@yandex.ru>
2023-10-21 04:56:08 +0300

    Update Sliding Window Maximum

--- solutions/sliding-window-maximum/solution.hpp
+++ solutions/sliding-window-maximum/solution.hpp
@@ -3,25 +3,44 @@
 #include <queue>
 #include <vector>
 
+template <typename ValueType, typename Comparator = std::less<>>
+class SlidingWindow {
+public:
+  explicit SlidingWindow(size_t size) : size_(size), comp_() {}
+
+  void Push(ValueType value) {
+    if (cnt_ >= size_) {
+      while (!deque_.empty() && deque_.front().first <= cnt_ - size_) {
+        deque_.pop_front();
+      }
+    }
+    while (!deque_.empty() && !comp_(value, deque_.back().second)) {
+      deque_.pop_back();
+    }
+    deque_.emplace_back(cnt_++, std::move(value));
+  }
+
+  const ValueType &Top() const { return deque_.front().second; }
+
+private:
+  std::deque<std::pair<size_t, ValueType>> deque_;
+  size_t size_;
+  size_t cnt_ = 0;
+  Comparator comp_;
+};
+
 class Solution {
 public:
   static std::vector<int> maxSlidingWindow(const std::vector<int> &nums,
                                            int k) {
-    const int n = nums.size();
-    std::deque<int> deque;
-    std::vector<int> maximums(n - k + 1);
-    for (int i = 0; i < n; ++i) {
-      while (!deque.empty() && deque.back() < nums[i]) {
-        deque.pop_back();
-      }
-      deque.push_back(nums[i]);
-      if (i >= k && nums[i - k] == deque.front()) {
-        deque.pop_front();
-      }
+    SlidingWindow<int> window(k);
+    std::vector<int> ans;
+    for (int i = 0; i < std::ssize(nums); ++i) {
+      window.Push(nums[i]);
       if (i >= k - 1) {
-        maximums[i - k + 1] = deque.front();
+        ans.push_back(window.Top());
       }
     }
-    return maximums;
+    return ans;
   }
 };
