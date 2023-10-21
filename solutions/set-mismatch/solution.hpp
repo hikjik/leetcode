7a3c7f63033596b096831f9c7e2b6407a3663524
Морев Александр <hikjik@yandex.ru>
2023-10-21 15:20:11 +0300

    Set Mismatch

---
+++ solutions/set-mismatch/solution.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int> findErrorNums(std::vector<int> nums) {
+    const int n = nums.size();
+
+    for (int i = 0; i < n;) {
+      if (nums[i] != nums[nums[i] - 1]) {
+        std::swap(nums[i], nums[nums[i] - 1]);
+      } else {
+        ++i;
+      }
+    }
+
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] != i + 1) {
+        return {nums[i], i + 1};
+      }
+    }
+
+    throw;
+  }
+};
