a98661c8a787aa5c461413cd45343962efa2741e
Морев Александр <hikjik@yandex.ru>
2023-10-21 03:15:06 +0300

    Binary Gap

--- solutions/binary-gap/solution.hpp
+++ solutions/binary-gap/solution.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
-
-
 class Solution {
 public:
-    static int binaryGap(int n) {
-        
+  static int binaryGap(int n) {
+    int gap = 0;
+    for (int x = n >> 1 + __builtin_ctz(n); x; x >>= 1 + __builtin_ctz(x)) {
+      gap = std::max(gap, 1 + __builtin_ctz(x));
     }
+    return gap;
+  }
 };
