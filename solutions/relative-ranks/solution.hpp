6d6192b5fb77f4f48507302e3b19cc9eeedefed9
Морев Александр <hikjik@yandex.ru>
2023-10-22 04:13:56 +0300

    Relative Ranks

---
+++ solutions/relative-ranks/solution.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<std::string>
+  findRelativeRanks(const std::vector<int> &score) {
+    std::vector<int> indexes(score.size());
+    std::iota(indexes.begin(), indexes.end(), 0);
+    std::sort(indexes.begin(), indexes.end(),
+              [&](int i, int j) { return score[i] > score[j]; });
+
+    std::vector<std::string> ans(score.size());
+    for (int i = 0; i < std::ssize(score); ++i) {
+      ans[indexes[i]] = getRank(i);
+    }
+    return ans;
+  }
+
+private:
+  static std::string getRank(int i) {
+    switch (i) {
+    case 0:
+      return "Gold Medal";
+    case 1:
+      return "Silver Medal";
+    case 2:
+      return "Bronze Medal";
+    default:
+      return std::to_string(i + 1);
+    }
+  }
+};
