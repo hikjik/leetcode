50ef591f33556a99936b102edfa38b20b6abe329
Морев Александр <hikjik@yandex.ru>
2023-10-22 07:42:17 +0300

    Minimum Distance to the Target Element

---
+++ solutions/minimum-distance-to-the-target-element/solution.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cmath>
+#include <vector>
+
+class Solution {
+public:
+  static int getMinDistance(const std::vector<int> &nums, int target,
+                            int start) {
+    const int n = nums.size();
+    auto distance = n - 1;
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] == target) {
+        distance = std::min(distance, std::abs(start - i));
+      }
+    }
+    return distance;
+  }
+};
