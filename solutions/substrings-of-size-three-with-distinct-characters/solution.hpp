ef3ff2ac3d9a3f9c713f26b8715f34b0f0018460
Морев Александр <hikjik@yandex.ru>
2023-10-22 13:58:05 +0300

    Substrings of Size Three with Distinct Characters

---
+++ solutions/substrings-of-size-three-with-distinct-characters/solution.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  static int countGoodSubstrings(std::string s) {
+    int cnt = 0;
+    for (int i = 0; i + 2 < std::ssize(s); ++i) {
+      if (s[i] != s[i + 1] && s[i] != s[i + 2] && s[i + 1] != s[i + 2]) {
+        ++cnt;
+      }
+    }
+    return cnt;
+  }
+};
