fdbeeafb0d9bf1ee16b07df746508c8e7af3baf8
Морев Александр <hikjik@yandex.ru>
2023-10-22 07:38:41 +0300

    Replace All Digits with Characters

---
+++ solutions/replace-all-digits-with-characters/solution.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  static std::string replaceDigits(std::string s) {
+    for (int i = 1; i < std::ssize(s); i += 2) {
+      s[i] = s[i - 1] + s[i] - '0';
+    }
+    return s;
+  }
+};
