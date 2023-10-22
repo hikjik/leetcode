6ec32d320753a70e690ace897be6c5f2552f1ca1
Морев Александр <hikjik@yandex.ru>
2023-10-22 07:24:42 +0300

    Minimum Operations to Make the Array Increasing

---
+++ solutions/minimum-operations-to-make-the-array-increasing/solution.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static int minOperations(const std::vector<int> &nums) {
+    int ops = 0;
+    for (int prev = 0; auto a : nums) {
+      ops += std::max(0, prev - a + 1);
+      prev = std::max(a, prev + 1);
+    }
+    return ops;
+  }
+};
