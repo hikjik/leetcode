c7d0a6d7b29bb26424a80615a8c200fbd4e1153a
Морев Александр <hikjik@yandex.ru>
2023-10-23 19:20:00 +0300

    Finding 3-Digit Even Numbers

---
+++ solutions/finding-3-digit-even-numbers/solution.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <array>
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int> findEvenNumbers(const std::vector<int> &digits) {
+    std::array<int, 10> counter{};
+    for (auto d : digits) {
+      ++counter[d];
+    }
+
+    std::vector<int> ans;
+    for (int a = 1; a < 10; ++a) {
+      for (int b = 0; b < 10; ++b) {
+        for (int c = 0; c < 10; c += 2) {
+          if (counter[a] && counter[b] > (a == b) &&
+              counter[c] > (a == c) + (b == c)) {
+            ans.push_back(a * 100 + b * 10 + c);
+          }
+        }
+      }
+    }
+    return ans;
+  }
+};
