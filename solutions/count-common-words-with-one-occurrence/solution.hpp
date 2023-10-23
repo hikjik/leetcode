d3a11f520aefbb3857c8a189ba17e1e7e3dca926
Морев Александр <hikjik@yandex.ru>
2023-10-23 17:25:33 +0300

    Count Common Words With One Occurrence

---
+++ solutions/count-common-words-with-one-occurrence/solution.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+  static int countWords(const std::vector<std::string> &words1,
+                        const std::vector<std::string> &words2) {
+    std::unordered_map<std::string, int> counter;
+    for (const auto &word : words1) {
+      ++counter[word];
+    }
+
+    for (const auto &word : words2) {
+      if (auto it = counter.find(word); it != counter.end() && it->second < 2) {
+        --it->second;
+      }
+    }
+
+    return std::count_if(counter.begin(), counter.end(),
+                         [](const auto &p) { return p.second == 0; });
+  }
+};
