9c85510e2fb4539f845c7be1d837135df768be3f
Морев Александр <hikjik@yandex.ru>
2023-10-23 16:29:15 +0300

    Minimum Cost of Buying Candies With Discount

---
+++ solutions/minimum-cost-of-buying-candies-with-discount/solution.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+class Solution {
+public:
+  static int minimumCost(std::vector<int> cost) {
+    std::sort(cost.begin(), cost.end(), std::greater<>());
+    int sum = std::accumulate(cost.begin(), cost.end(), 0);
+    for (size_t i = 2; i < cost.size(); i += 3) {
+      sum -= cost[i];
+    }
+    return sum;
+  }
+};
