ad351844dd8d0cdcc3f5ace1111a194a276e971a
Морев Александр <hikjik@yandex.ru>
2023-10-23 21:55:15 +0300

    Final Prices With a Special Discount in a Shop

---
+++ solutions/final-prices-with-a-special-discount-in-a-shop/solution.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stack>
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int> finalPrices(const std::vector<int> &prices) {
+    std::vector<int> final(prices);
+    std::stack<int> stack;
+    for (int i = prices.size() - 1; i >= 0; --i) {
+      while (!stack.empty() && stack.top() > prices[i]) {
+        stack.pop();
+      }
+      final[i] -= stack.empty() ? 0 : stack.top();
+      stack.push(prices[i]);
+    }
+    return final;
+  }
+};
