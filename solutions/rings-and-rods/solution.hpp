71d45611ecbea8bac0a213b32faf079d42a84ae4
Морев Александр <hikjik@yandex.ru>
2023-10-23 16:43:26 +0300

    Rings and Rods

---
+++ solutions/rings-and-rods/solution.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <array>
+#include <string>
+
+class Solution {
+public:
+  static int countPoints(std::string rings) {
+    std::array<int, 10> rods{};
+    for (size_t i = 0; i < rings.size(); i += 2) {
+      const auto color = rings[i] == 'R' ? 1 : rings[i] == 'B' ? 2 : 4;
+      rods[rings[i + 1] - '0'] |= color;
+    }
+    return std::count(rods.begin(), rods.end(), 7);
+  }
+};
