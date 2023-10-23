a1e1cf7655d7cff5d358faceecf8cc8e0cb360c3
Морев Александр <hikjik@yandex.ru>
2023-10-23 16:35:21 +0300

    Maximum Number of Words Found in Sentences

---
+++ solutions/maximum-number-of-words-found-in-sentences/solution.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+  static int mostWordsFound(const std::vector<std::string> &sentences) {
+    int ans = 0;
+    for (const auto &s : sentences) {
+      const int words_count = std::count(s.begin(), s.end(), ' ') + 1;
+      ans = std::max(ans, words_count);
+    }
+    return ans;
+  }
+};
