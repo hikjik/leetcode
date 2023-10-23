f071a684ae8e9f9311c63061c3bf0c0069ab5744
Морев Александр <hikjik@yandex.ru>
2023-10-23 04:36:14 +0300

    Number Of Rectangles That Can Form The Largest Square

---
+++ solutions/number-of-rectangles-that-can-form-the-largest-square/solution.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static int
+  countGoodRectangles(const std::vector<std::vector<int>> &rectangles) {
+    int max_length = 0, cnt = 0;
+    for (const auto &rectangle : rectangles) {
+      const auto length = std::min(rectangle[0], rectangle[1]);
+      if (length > max_length) {
+        max_length = length;
+        cnt = 1;
+      } else if (length == max_length) {
+        ++cnt;
+      }
+    }
+    return cnt;
+  }
+};
