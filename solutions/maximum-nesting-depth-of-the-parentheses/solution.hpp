2cdc60e075bc3174d0c0d3de884d180ac92f40a9
Морев Александр <hikjik@yandex.ru>
2023-10-23 21:29:33 +0300

    Maximum Nesting Depth of the Parentheses

---
+++ solutions/maximum-nesting-depth-of-the-parentheses/solution.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  static int maxDepth(std::string s) {
+    int max_depth = 0;
+    for (int depth = 0; auto c : s) {
+      if (c == '(') {
+        max_depth = std::max(max_depth, ++depth);
+      } else if (c == ')') {
+        --depth;
+      }
+    }
+    return max_depth;
+  }
+};
