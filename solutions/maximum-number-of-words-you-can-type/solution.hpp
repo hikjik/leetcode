04914914cca2d7056d02a3197cbf0ea89002e81e
Морев Александр <hikjik@yandex.ru>
2023-10-23 14:17:16 +0300

    Maximum Number of Words You Can Type

---
+++ solutions/maximum-number-of-words-you-can-type/solution.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <unordered_set>
+
+class Solution {
+public:
+  static int canBeTypedWords(std::string text, std::string brokenLetters) {
+    std::unordered_set<char> broken(brokenLetters.begin(), brokenLetters.end());
+    std::istringstream stream(text);
+    return std::count_if(
+        std::istream_iterator<std::string>(stream),
+        std::istream_iterator<std::string>(), [&](const std::string &word) {
+          return std::all_of(word.begin(), word.end(),
+                             [&](char c) { return !broken.contains(c); });
+        });
+  }
+};
