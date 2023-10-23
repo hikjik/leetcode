c8fc370f5529992d13838ce2ab5af67ecb9ae057
Морев Александр <hikjik@yandex.ru>
2023-10-23 16:18:13 +0300

    Capitalize the Title

---
+++ solutions/capitalize-the-title/solution.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  static std::string capitalizeTitle(std::string title) {
+    for (size_t i = 0; i < title.size(); ++i) {
+      if ((!i || title[i - 1] == ' ') && i + 2 < title.size() &&
+          title.substr(i + 1, 2).find(' ') == std::string::npos) {
+        title[i] = std::toupper(title[i]);
+      } else {
+        title[i] = std::tolower(title[i]);
+      }
+    }
+    return title;
+  }
+};
