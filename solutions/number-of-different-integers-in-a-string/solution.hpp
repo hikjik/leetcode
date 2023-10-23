14163ab4de8e3a203d0ff40b88f6d55f12e14a87
Морев Александр <hikjik@yandex.ru>
2023-10-23 09:27:46 +0300

    Number of Different Integers in a String

---
+++ solutions/number-of-different-integers-in-a-string/solution.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <unordered_set>
+
+class Solution {
+public:
+  static int numDifferentIntegers(std::string word) {
+    std::unordered_set<std::string> set;
+    for (auto first = word.begin(); first != word.end();) {
+      if (!std::isdigit(*first)) {
+        ++first;
+      } else {
+        auto last = std::find_if(first, word.end(),
+                                 [](char c) { return !std::isdigit(c); });
+        while (first != std::prev(last) && *first == '0') {
+          ++first;
+        }
+        set.emplace(first, last);
+        first = last;
+      }
+    }
+    return set.size();
+  }
+};
