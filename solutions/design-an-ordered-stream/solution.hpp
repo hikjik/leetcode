4a1007be422e61e50fb94e01e73ad4e4fc8aa067
Морев Александр <hikjik@yandex.ru>
2023-10-23 22:02:24 +0300

    Design an Ordered Stream

---
+++ solutions/design-an-ordered-stream/solution.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class OrderedStream {
+public:
+  OrderedStream(int n) : values_(n), pos_(0) {}
+
+  std::vector<std::string> insert(int idKey, std::string value) {
+    values_[idKey - 1] = value;
+    std::vector<std::string> chunk;
+    while (pos_ < values_.size() && !values_[pos_].empty()) {
+      chunk.push_back(values_[pos_++]);
+    }
+    return chunk;
+  }
+
+private:
+  std::vector<std::string> values_;
+  size_t pos_;
+};
