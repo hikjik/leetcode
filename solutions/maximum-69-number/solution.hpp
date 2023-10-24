2c7f4ada44d7ac28fac099ee79cce06772d9ec2c
Морев Александр <hikjik@yandex.ru>
2023-10-24 19:47:42 +0300

    Maximum 69 Number

---
+++ solutions/maximum-69-number/solution.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  static int maximum69Number(int num) {
+    auto str = std::to_string(num);
+    if (auto pos = str.find_first_of('6'); pos != std::string::npos) {
+      str[pos] = '9';
+    }
+    return std::stoi(str);
+  }
+};
