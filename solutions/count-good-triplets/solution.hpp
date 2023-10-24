273aa647ed3febfcc4fd5703e80d7f962da3dd5c
Морев Александр <hikjik@yandex.ru>
2023-10-24 11:08:30 +0300

    Count Good Triplets

---
+++ solutions/count-good-triplets/solution.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cmath>
+#include <vector>
+
+class Solution {
+public:
+  static int countGoodTriplets(const std::vector<int> &arr, int a, int b,
+                               int c) {
+    int cnt = 0;
+    for (size_t i = 0; i < arr.size(); ++i) {
+      for (size_t j = i + 1; j < arr.size(); ++j) {
+        if (std::abs(arr[i] - arr[j]) <= a) {
+          for (size_t k = j + 1; k < arr.size(); ++k) {
+            cnt += std::abs(arr[j] - arr[k]) <= b &&
+                   std::abs(arr[i] - arr[k]) <= c;
+          }
+        }
+      }
+    }
+    return cnt;
+  }
+};
