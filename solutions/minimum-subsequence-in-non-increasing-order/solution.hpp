4b37473d749cf074b560ec87403c8b50af1715bd
Морев Александр <hikjik@yandex.ru>
2023-10-25 09:56:36 +0300

    Minimum Subsequence in Non-Increasing Order

---
+++ solutions/minimum-subsequence-in-non-increasing-order/solution.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int> minSubsequence(std::vector<int> nums) {
+    std::sort(nums.begin(), nums.end(), std::greater{});
+    const auto half = std::accumulate(nums.begin(), nums.end(), 0) / 2;
+    std::vector<int> ans;
+    for (int i = 0, sum = 0; sum <= half; sum += nums[i++]) {
+      ans.push_back(nums[i]);
+    }
+    return ans;
+  }
+};
