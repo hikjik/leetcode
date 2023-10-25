3c5356efaa74128e0174bb902d72eb18452c69a2
Морев Александр <hikjik@yandex.ru>
2023-10-25 15:13:13 +0300

    Reformat The String

---
+++ solutions/reformat-the-string/solution.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  static std::string reformat(std::string s) {
+    std::vector<int> cnt(2);
+    for (auto c : s) {
+      ++cnt[std::isdigit(c)];
+    }
+
+    if (std::abs(cnt[0] - cnt[1]) > 1) {
+      return "";
+    }
+
+    std::string ans(s);
+    for (int i = -2, j = -1; auto c : s) {
+      ans[std::isdigit(c) == cnt[1] >= cnt[0] ? i += 2 : j += 2] = c;
+    }
+    return ans;
+  }
+};
