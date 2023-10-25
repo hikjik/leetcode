ea2e1d00fced44d044bb1b2d6e5b9cbc85239308
Морев Александр <hikjik@yandex.ru>
2023-10-25 12:07:13 +0300

    Reformat Phone Number

---
+++ solutions/reformat-phone-number/solution.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+
+class Solution {
+public:
+  static std::string reformatNumber(std::string number) {
+    std::string ans;
+    for (int cnt = 0; auto c : number) {
+      if (std::isdigit(c)) {
+        if (cnt && cnt % 3 == 0) {
+          ans.push_back('-');
+        }
+        ++cnt;
+        ans.push_back(c);
+      }
+    }
+
+    if (ans[ans.size() - 2] == '-') {
+      std::swap(ans[ans.size() - 2], ans[ans.size() - 3]);
+    }
+    return ans;
+  }
+};
