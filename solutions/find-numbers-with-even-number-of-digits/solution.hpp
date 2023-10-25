13983328ece25ad6f86e6c684789185ecb62874c
Морев Александр <hikjik@yandex.ru>
2023-10-25 17:40:48 +0300

    Find Numbers with Even Number of Digits

---
+++ solutions/find-numbers-with-even-number-of-digits/solution.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+  static int findNumbers(const std::vector<int> &nums) {
+    return std::count_if(nums.begin(), nums.end(), [](int n) {
+      return std::to_string(n).size() % 2 == 0;
+    });
+  }
+};
