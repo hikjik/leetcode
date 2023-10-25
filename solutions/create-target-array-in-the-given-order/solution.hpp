6685b597e3f1da6992ac8503e7aaef32d9711849
Морев Александр <hikjik@yandex.ru>
2023-10-25 13:35:59 +0300

    Create Target Array in the Given Order

---
+++ solutions/create-target-array-in-the-given-order/solution.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int> createTargetArray(const std::vector<int> &nums,
+                                            const std::vector<int> &index) {
+    std::vector<int> ans;
+    for (size_t i = 0; i < nums.size(); ++i) {
+      ans.insert(ans.begin() + index[i], nums[i]);
+    }
+    return ans;
+  }
+};
