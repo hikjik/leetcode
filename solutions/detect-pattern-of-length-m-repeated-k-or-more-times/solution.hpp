c77a7a5a3127ef640d3a34022f2427e336ec6af2
Морев Александр <hikjik@yandex.ru>
2023-10-25 21:50:15 +0300

    Detect Pattern of Length M Repeated K or More Times

---
+++ solutions/detect-pattern-of-length-m-repeated-k-or-more-times/solution.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+public:
+  static bool containsPattern(const std::vector<int> &arr, int m, int k) {
+    for (int i = 0, cnt = 0; i + m < std::ssize(arr); ++i) {
+      if (arr[i] != arr[i + m]) {
+        cnt = 0;
+      } else if (++cnt == (k - 1) * m) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
