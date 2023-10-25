771c8c39be7d5a31ecabafa4d9870567c045b1c3
Морев Александр <hikjik@yandex.ru>
2023-10-25 11:46:09 +0300

    Count the Number of Consistent Strings

---
+++ solutions/count-the-number-of-consistent-strings/solution.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <ranges>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+public:
+  static int countConsistentStrings(std::string allowed,
+                                    const std::vector<std::string> &words) {
+    std::unordered_set<char> set(allowed.begin(), allowed.end());
+    return std::ranges::count_if(words, [&](const auto &word) {
+      return std::ranges::all_of(word, [&](char c) { return set.contains(c); });
+    });
+  }
+};
