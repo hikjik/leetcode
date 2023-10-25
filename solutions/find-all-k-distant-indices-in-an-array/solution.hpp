9eea3d9f16e0197c32defcbda9913ce4a66dc696
Морев Александр <hikjik@yandex.ru>
2023-10-25 20:00:27 +0300

    Find All K-Distant Indices in an Array

---
+++ solutions/find-all-k-distant-indices-in-an-array/solution.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+  static std::vector<int> findKDistantIndices(const std::vector<int> &nums,
+                                              int key, int k) {
+    std::vector<int> ans;
+    auto cnt = std::count(nums.begin(), nums.begin() + k, key);
+    for (int i = 0; i < std::ssize(nums); ++i) {
+      if (i + k < std::ssize(nums)) {
+        cnt += nums[i + k] == key;
+      }
+      if (cnt) {
+        ans.push_back(i);
+      }
+      if (i >= k) {
+        cnt -= nums[i - k] == key;
+      }
+    }
+    return ans;
+  }
+};
