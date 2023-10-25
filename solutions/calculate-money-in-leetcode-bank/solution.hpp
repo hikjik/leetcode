3ffce7f55fdecbada07348d0a49cf36d3ae72bcb
Морев Александр <hikjik@yandex.ru>
2023-10-25 13:12:20 +0300

    Calculate Money in Leetcode Bank

---
+++ solutions/calculate-money-in-leetcode-bank/solution.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+class Solution {
+public:
+  static int totalMoney(int n) {
+    const int q = n / 7, r = n % 7;
+    return q * sum(7) + 7 * sum(q - 1) + q * r + sum(r);
+  }
+
+private:
+  static int sum(int n) { return (n + 1) * n / 2; }
+};
