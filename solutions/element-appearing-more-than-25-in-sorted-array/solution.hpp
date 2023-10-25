bd7687423e4a12b8cfa37a840cce48d9ae7cdf43
Морев Александр <hikjik@yandex.ru>
2023-10-25 17:37:32 +0300

    Element Appearing More Than 25% In Sorted Array

---
+++ solutions/element-appearing-more-than-25-in-sorted-array/solution.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+  static int findSpecialInteger(const std::vector<int> &arr) {
+    const int n = arr.size();
+    for (int i = 0; i < n; i += n / 4) {
+      auto [first, last] = std::equal_range(arr.begin(), arr.end(), arr[i]);
+      if (std::distance(first, last) > n / 4) {
+        return arr[i];
+      }
+    }
+    throw;
+  }
+};
