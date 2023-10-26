31cf2f74c172cb042f5397fb42415472d483ff47
Морев Александр <hikjik@yandex.ru>
2023-10-26 11:57:22 +0300

    Binary Trees With Factors

---
+++ solutions/binary-trees-with-factors/solution.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <vector>
+
+class Solution {
+public:
+  static constexpr int kMod = 1e9 + 7;
+
+  static int numFactoredBinaryTrees(std::vector<int> arr) {
+    std::sort(arr.begin(), arr.end());
+
+    std::unordered_map<int, int> indexes;
+    for (int i = 0; i < std::ssize(arr); ++i) {
+      indexes[arr[i]] = i;
+    }
+
+    std::vector<long long> dp(arr.size(), 1);
+    for (int i = 1; i < std::ssize(arr); ++i) {
+      for (int j = 0; j < i; ++j) {
+        if (arr[j] > std::sqrt(arr[i])) {
+          break;
+        }
+        if (arr[i] % arr[j] != 0) {
+          continue;
+        }
+        if (auto it = indexes.find(arr[i] / arr[j]); it != indexes.end()) {
+          const int k = it->second;
+          dp[i] += (k == j ? 1 : 2) * dp[j] * dp[k];
+          dp[i] %= kMod;
+        }
+      }
+    }
+
+    return std::accumulate(dp.begin(), dp.end(), 0LL) % kMod;
+  }
+};
