45aec2beaf8cb81e638f7139708e0957aecc06c1
Морев Александр <hikjik@yandex.ru>
2023-10-29 03:50:23 +0300

    Subarrays Distinct Element Sum of Squares I

---
+++ solutions/subarrays-distinct-element-sum-of-squares-i/solution.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+public:
+  static int sumCounts(const std::vector<int> &nums) {
+    int ans = 0;
+    for (size_t i = 0; i < nums.size(); ++i) {
+      std::unordered_set<int> set;
+      for (size_t j = i; j < nums.size(); ++j) {
+        set.insert(nums[j]);
+        ans += set.size() * set.size();
+      }
+    }
+    return ans;
+  }
+};
