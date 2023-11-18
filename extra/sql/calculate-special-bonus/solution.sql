945e9bcc8341c5c2184b0b70a6666eb7eb6514d6
Морев Александр <hikjik@yandex.ru>
2023-11-19 00:43:11 +0300

    Calculate Special Bonus

---
+++ extra/sql/calculate-special-bonus/solution.sql
@@ -0,0 +1,8 @@
+SELECT employee_id,
+       CASE
+           WHEN employee_id % 2 = 1 AND LEFT(name, 1) != 'M'
+           THEN salary
+           ELSE 0
+       END bonus
+  FROM Employees
+ ORDER BY employee_id;
