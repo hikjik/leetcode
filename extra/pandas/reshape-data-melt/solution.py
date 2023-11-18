4d3ef6aa22b71953854f3480b2fe0e53ab7899e4
Морев Александр <hikjik@yandex.ru>
2023-11-19 01:12:24 +0300

    Reshape Data: Pivot

--- extra/pandas/reshape-data-melt/solution.py
+++ extra/pandas/reshape-data-melt/solution.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 
 def meltTable(report: pd.DataFrame) -> pd.DataFrame:
-    return pd.melt(report, id_vars=["product"], var_name="quarter", value_name="sales")
+    return report.melt(id_vars=["product"], var_name="quarter", value_name="sales")
