0b0e1fa35ebbf6dc7e61432991019c362f2b9d0a
Морев Александр <hikjik@yandex.ru>
2023-11-18 13:50:26 +0300

    Project Employees I

---
+++ extra/pandas/project-employees-i/solution.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
+    return (
+        project.merge(employee, on="employee_id")
+        .groupby("project_id")
+        .experience_years.mean()
+        .round(decimals=2)
+        .reset_index(name="average_years")
+    )
---
+++ extra/sql/project-employees-i/solution.sql
@@ -0,0 +1,6 @@
+SELECT project_id,
+       ROUND(AVG(experience_years), 2) AS average_years
+  FROM Project
+ INNER JOIN Employee
+    ON Project.employee_id = Employee.employee_id
+ GROUP BY project_id;
