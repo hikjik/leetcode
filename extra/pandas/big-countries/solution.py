ed41acd9a5a9636fbcd34c1692693dd017e2785a
Морев Александр <hikjik@yandex.ru>
2023-11-18 08:33:39 +0300

    Big Countries

---
+++ extra/pandas/big-countries/solution.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    return world.query("area >= 3000000 or population >= 25000000")[
+        ["name", "population", "area"]
+    ]
