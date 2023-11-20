9c7626eafda8f71e263a273bc6a3185dbc397536
Морев Александр <hikjik@yandex.ru>
2023-11-20 08:31:18 +0300

    Update Range Sum Query - Immutable

--- solutions/range-sum-query-immutable/solution.hpp
+++ solutions/range-sum-query-immutable/solution.hpp
@@ -1,22 +1,21 @@
 #pragma once
 
+#include <numeric>
 #include <vector>
 
-// Time:
-// Space:
+// Time: O(N+M)
+// Space: O(N)
 
 class NumArray {
 public:
-  NumArray(std::vector<int> nums) : prefix_sums_(nums.size() + 1, 0) {
-    for (size_t i = 1; i < prefix_sums_.size(); ++i) {
-      prefix_sums_[i] = prefix_sums_[i - 1] + nums[i - 1];
-    }
+  NumArray(const std::vector<int> &nums) : prefix_sum(nums.size() + 1, 0) {
+    std::partial_sum(nums.begin(), nums.end(), prefix_sum.begin() + 1);
   }
 
   int sumRange(int left, int right) {
-    return prefix_sums_[right + 1] - prefix_sums_[left];
+    return prefix_sum[right + 1] - prefix_sum[left];
   }
 
 private:
-  std::vector<int> prefix_sums_;
+  std::vector<int> prefix_sum;
 };
