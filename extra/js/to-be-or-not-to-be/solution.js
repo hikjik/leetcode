49477e0a96f1c9c4882b3e0fc1d906cbe63a8df9
Морев Александр <hikjik@yandex.ru>
2023-11-26 01:38:30 +0300

    To Be Or Not To Be

---
+++ extra/js/to-be-or-not-to-be/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+    return {
+        toBe: function (v) {
+            if (v === val) {
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+        notToBe: function (v) {
+            if (v !== val) {
+                return true;
+            }
+            throw new Error("Equal");
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
