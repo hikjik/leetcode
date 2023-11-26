01d197b50dda06d9219b36b53abdd21b0382d97b
Морев Александр <hikjik@yandex.ru>
2023-11-27 01:01:42 +0300

    Chunk Array

---
+++ extra/js/chunk-array/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function (arr, size) {
+    let ans = [];
+    for (let i = 0; i < arr.length; i += size) {
+        ans.push(arr.slice(i, i + size));
+
+    }
+    return ans;
+};
