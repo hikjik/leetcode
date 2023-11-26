c5341170fea44232132d5630d5343b4239a9c2bb
Морев Александр <hikjik@yandex.ru>
2023-11-27 02:16:15 +0300

    Array Wrapper

---
+++ extra/js/array-wrapper/solution.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function (nums) {
+    this.sum = nums.reduce((a, b) => a + b, 0);
+    this.repr = JSON.stringify(nums);
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function () {
+    return this.sum;
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function () {
+    return this.repr;
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
