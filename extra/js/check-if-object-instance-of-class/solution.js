3101dea5003646b85d6f5168f748e704eda51803
Морев Александр <hikjik@yandex.ru>
2023-11-28 12:09:08 +0300

    Check if Object Instance of Class

---
+++ extra/js/check-if-object-instance-of-class/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function (obj, classFunction) {
+    if (obj === null || obj === undefined || typeof classFunction !== 'function') {
+        return false;
+    }
+    return Object(obj) instanceof classFunction;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
