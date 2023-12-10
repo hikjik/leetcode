12a11c0f28e8d5945815f40e04624c4ef195b163
Морев Александр <hikjik@yandex.ru>
2023-12-10 11:51:15 +0300

    Update Meta

--- leetcode/leetcode/cli.py
+++ leetcode/leetcode/cli.py
@@ -1,9 +1,15 @@
 import typing as tp
+from pathlib import Path
 
 import click
 
 from .client import Client
-from .templates import create_problem_folder, parse_problem, update_readme_table
+from .templates import (
+    create_meta_file,
+    create_problem_folder,
+    parse_problem,
+    update_readme_table,
+)
 
 
 def get_problem_slug_from_list(json: dict[str, tp.Any], id: int) -> str:
@@ -73,3 +79,36 @@ def pull(problem_id: int, slug: str, lang: str, daily: bool) -> None:
 @cli.command()
 def update_readme() -> None:
     update_readme_table()
+
+
+@cli.command()
+@click.argument(
+    "problem_id_from",
+    type=int,
+    required=True,
+)
+@click.argument(
+    "problem_id_to",
+    type=int,
+    required=True,
+)
+def update_meta(problem_id_from: int, problem_id_to: int) -> None:
+    client = Client()
+
+    for problem_id in range(problem_id_from, problem_id_to + 1):
+        try:
+            if problem_id is not None:
+                data = client.question_list(limit=1, skip=problem_id - 1)
+                slug = get_problem_slug_from_list(data, problem_id)
+
+            if slug is None:
+                ctx = click.get_current_context()
+                ctx.fail(ctx.get_help())
+
+            data = client.question_data(slug)
+            problem = parse_problem(data)
+            path = Path("solutions") / Path(problem.title_slug)
+            if Path.exists(path):
+                create_meta_file(path, problem)
+        except TypeError:
+            pass
--- leetcode/leetcode/templates/__init__.py
+++ leetcode/leetcode/templates/__init__.py
@@ -1,4 +1,5 @@
 from .folder import create_problem_folder
+from .meta import create_meta_file
 from .problem import parse_problem
 from .readme import update_readme_table
 
@@ -6,4 +7,5 @@ __all__ = [
     "create_problem_folder",
     "update_readme_table",
     "parse_problem",
+    "create_meta_file",
 ]
