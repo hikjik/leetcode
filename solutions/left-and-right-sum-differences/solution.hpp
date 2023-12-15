f3725605899b661e89133eef51c437b1b26ed6ba
Морев Александр <hikjik@yandex.ru>
2023-12-15 13:05:00 +0300

    Left and Right Sum Differences

--- solutions/left-and-right-sum-differences/solution.hpp
+++ solutions/left-and-right-sum-differences/solution.hpp
@@ -1,29 +1,22 @@
 #pragma once
 
+#include <cmath>
+#include <numeric>
 #include <vector>
 
-// Time:
-// Space:
+// Time:  O(N)
+// Space: O(1)
 
 class Solution {
 public:
   static std::vector<int> leftRightDifference(const std::vector<int> &nums) {
-    size_t n = nums.size();
-
-    std::vector<int> left(n, 0);
-    for (size_t i = 1; i < n; ++i) {
-      left[i] = left[i - 1] + nums[i - 1];
+    auto left = 0, right = std::reduce(nums.begin(), nums.end(), 0);
+    std::vector<int> ans;
+    for (auto num : nums) {
+      right -= num;
+      ans.push_back(std::abs(right - left));
+      left += num;
     }
-
-    std::vector<int> rights(n, 0);
-    for (int i = n - 1; i > 0; --i) {
-      rights[i - 1] = rights[i] + nums[i];
-    }
-
-    std::vector<int> diff(n, 0);
-    for (size_t i = 0; i < n; ++i) {
-      diff[i] = abs(rights[i] - left[i]);
-    }
-    return diff;
+    return ans;
   }
 };
