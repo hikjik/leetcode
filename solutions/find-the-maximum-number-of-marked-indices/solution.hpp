66eb4483ef0a4bade52f3dfb94ea30afb2943c09
Морев Александр <hikjik@yandex.ru>
2023-12-15 13:20:31 +0300

    Find the Maximum Number of Marked Indices

--- solutions/find-the-maximum-number-of-marked-indices/solution.hpp
+++ solutions/find-the-maximum-number-of-marked-indices/solution.hpp
@@ -1,25 +1,24 @@
 #pragma once
 
-#include <algorithm>
+#include <ranges>
 #include <vector>
 
-// Time:
-// Space:
+// Time: O(NlogN)
+// Space: O(N)
 
 class Solution {
 public:
   static int maxNumOfMarkedIndices(std::vector<int> nums) {
-    std::sort(nums.begin(), nums.end());
+    std::ranges::sort(nums);
 
-    auto end = nums.end();
-    auto begin = nums.begin() + nums.size() / 2;
-    int total = 0;
-    for (int i = nums.size() / 2 - 1; i >= 0; --i) {
-      if (auto it = std::lower_bound(begin, end, 2 * nums[i]); it != end) {
-        total += 2;
-        end = std::prev(end);
+    int ans = 0;
+    const int n = nums.size();
+    for (int j = n - 1, i = n / 2 - 1; i >= 0; --i) {
+      if (2 * nums[i] <= nums[j]) {
+        ans += 2;
+        --j;
       }
     }
-    return total;
+    return ans;
   }
 };
