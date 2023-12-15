53dde84234c17c6b24bd78d28508fb3a87deaf98
Морев Александр <hikjik@yandex.ru>
2023-12-15 13:12:21 +0300

    Find the Divisibility Array of a String]

--- solutions/find-the-divisibility-array-of-a-string/solution.hpp
+++ solutions/find-the-divisibility-array-of-a-string/solution.hpp
@@ -3,23 +3,17 @@
 #include <string>
 #include <vector>
 
-// Time:
-// Space:
+// Time: O(N)
+// Space: O(1)
 
 class Solution {
 public:
   static std::vector<int> divisibilityArray(std::string word, int m) {
-    std::vector<int> divisibility_array(word.size(), 0);
-
-    long long sum = 0;
-    for (size_t i = 0; i < word.size(); ++i) {
-      sum %= m;
-      sum = sum * 10 + word[i] - '0';
-      if (sum % m == 0) {
-        divisibility_array[i] = 1;
-      }
+    std::vector<int> ans;
+    for (long long num = 0; auto c : word) {
+      num = (num * 10 + c - '0') % m;
+      ans.push_back(num ? 0 : 1);
     }
-
-    return divisibility_array;
+    return ans;
   }
 };
