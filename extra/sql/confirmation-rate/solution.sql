fb32d77f24110b079980415a4ee8f9738518ff71
Морев Александр <hikjik@yandex.ru>
2024-01-05 17:48:04 +0300

    Confirmation Rate

--- extra/sql/confirmation-rate/solution.sql
+++ extra/sql/confirmation-rate/solution.sql
@@ -1,1 +1,6 @@
-# Write your MySQL query statement below
+SELECT Signups.user_id,
+       ROUND(AVG(IF(action = "confirmed", 1, 0)), 2) AS confirmation_rate
+  FROM Signups
+  LEFT JOIN Confirmations
+    ON Signups.user_id = Confirmations.user_id
+ GROUP BY Signups.user_id;
