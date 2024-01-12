f234be2078e1dd7a136db07d549b06a95f475279
Морев Александр <hikjik@yandex.ru>
2024-01-12 03:26:58 +0300

    Determine if String Halves Are Alike

--- solutions/determine-if-string-halves-are-alike/solution.hpp
+++ solutions/determine-if-string-halves-are-alike/solution.hpp
@@ -1,27 +1,19 @@
 #pragma once
 
+#include <algorithm>
 #include <string>
-#include <unordered_set>
 
-// Time:
-// Space:
+// Time: O(N)
+// Space: O(1)
 
 class Solution {
 public:
   static bool halvesAreAlike(std::string s) {
-    int cnt = 0;
-    for (size_t i = 0; i < s.size(); ++i) {
-      if (isVowel(s[i])) {
-        cnt += i < s.size() / 2 ? 1 : -1;
-      }
-    }
-    return cnt == 0;
-  }
-
-private:
-  static bool isVowel(char c) {
-    static const std::unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u',
-                                                 'A', 'E', 'I', 'O', 'U'};
-    return vowels.contains(c);
+    const auto isVowel = [](char c) {
+      static const std::string vowels = "aeiouAEIOU";
+      return vowels.find(c) != std::string::npos;
+    };
+    return std::count_if(s.cbegin(), s.cbegin() + s.size() / 2, isVowel) ==
+           std::count_if(s.crbegin(), s.crbegin() + s.size() / 2, isVowel);
   }
 };
