c1079372803dad26d9bb9eb6ec31970fead01e90
Морев Александр <hikjik@yandex.ru>
2024-01-17 03:18:16 +0300

    Update Daily

--- solutions/unique-number-of-occurrences/solution.hpp
+++ solutions/unique-number-of-occurrences/solution.hpp
@@ -4,22 +4,22 @@
 #include <unordered_set>
 #include <vector>
 
-// Time:
-// Space:
+// Time: O(N)
+// Space: O(N)
 
 class Solution {
 public:
   static bool uniqueOccurrences(const std::vector<int> &arr) {
-    std::unordered_map<int, int> map;
-    for (auto a : arr) {
-      ++map[a];
+    std::unordered_map<int, int> counter;
+    for (auto num : arr) {
+      ++counter[num];
     }
+
     std::unordered_set<int> set;
-    for (const auto &[a, f] : map) {
-      if (set.count(f)) {
+    for (const auto &[_, cnt] : counter) {
+      if (!set.insert(cnt).second) {
         return false;
       }
-      set.insert(f);
     }
     return true;
   }
