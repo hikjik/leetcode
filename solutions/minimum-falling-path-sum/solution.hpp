09dd962dbe6a77033a296cd7c2407bb8e2dbbfaa
Морев Александр <hikjik@yandex.ru>
2024-01-19 03:52:09 +0300

    Update Daily

--- solutions/minimum-falling-path-sum/solution.hpp
+++ solutions/minimum-falling-path-sum/solution.hpp
@@ -1,26 +1,24 @@
 #pragma once
 
-#include <algorithm>
-#include <limits>
+#include <climits>
+#include <ranges>
 #include <vector>
 
-// Time:
-// Space:
+// Time: O(N^2)
+// Space: O(N)
 
 class Solution {
 public:
   static int minFallingPathSum(const std::vector<std::vector<int>> &matrix) {
     const int n = matrix.size();
-
-    std::vector<int> dp(n + 2, 0);
-    dp[0] = dp[n + 1] = std::numeric_limits<int>::max();
-    for (int i = n - 1; i >= 0; --i) {
+    std::vector<int> dp(n + 2);
+    dp[0] = dp[n + 1] = INT_MAX;
+    for (const auto &row : matrix) {
       std::vector<int> prev(dp);
-      for (int j = 1; j < n + 1; ++j) {
-        dp[j] =
-            std::min({prev[j - 1], prev[j], prev[j + 1]}) + matrix[i][j - 1];
+      for (int j = 0; j < n; ++j) {
+        dp[j + 1] = row[j] + std::min({prev[j], prev[j + 1], prev[j + 2]});
       }
     }
-    return *std::min_element(dp.begin(), dp.end());
+    return std::ranges::min(dp);
   }
 };
