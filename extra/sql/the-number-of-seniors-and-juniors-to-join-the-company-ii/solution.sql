6e849b9ded6942d11d69edc1d54fadaaab48c803
Морев Александр <hikjik@yandex.ru>
2024-01-21 02:14:06 +0300

    The Number of Seniors and Juniors to Join the Company II

---
+++ extra/sql/the-number-of-seniors-and-juniors-to-join-the-company-ii/solution.sql
@@ -0,0 +1,29 @@
+WITH CumulativeSalaries AS (
+    SELECT employee_id, experience,
+           SUM(salary) OVER(
+               PARTITION BY experience
+                   ORDER BY salary, employee_id) AS cumsum_salary
+      FROM Candidates
+), SenoirsBudget AS (
+    SELECT 70000 AS budget
+), SeniorsCost AS (
+    SELECT MAX(cumsum_salary) AS cost
+      FROM CumulativeSalaries, SenoirsBudget
+     WHERE experience = 'Senior' AND cumsum_salary <= budget
+), JuniorsBudget AS (
+    SELECT budget - IFNULL(cost, 0) AS budget
+      FROM SeniorsCost, SenoirsBudget
+), HiredSeniors AS (
+    SELECT employee_id
+      FROM CumulativeSalaries, SenoirsBudget
+     WHERE experience = 'Senior' AND cumsum_salary <= budget
+), HiredJuniors AS (
+    SELECT employee_id
+      FROM CumulativeSalaries, JuniorsBudget
+     WHERE experience = 'Junior' AND cumsum_salary <= budget
+)
+SELECT employee_id
+  FROM HiredSeniors
+ UNION ALL
+SELECT employee_id
+  FROM HiredJuniors;
