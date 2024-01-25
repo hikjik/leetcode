d945ee9d15e4723f1b555f271351a74999b26ced
Морев Александр <hikjik@yandex.ru>
2024-01-25 04:04:55 +0300

    Update Daily

--- solutions/longest-common-subsequence/solution.hpp
+++ solutions/longest-common-subsequence/solution.hpp
@@ -1,25 +1,90 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <vector>
 
-// Time:
-// Space:
+// Time: O(NM)
+// Space: O(NM)
 
+namespace memo {
+
+// Time: O(NM)
+// Space: O(NM)
 class Solution {
 public:
   static int longestCommonSubsequence(std::string text1, std::string text2) {
-    int n = text1.size(), m = text2.size();
-    std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, 0));
-    for (int i = 1; i < n + 1; i++) {
-      for (int j = 1; j < m + 1; j++) {
+    std::vector memo(text1.size(), std::vector<int>(text2.size(), -1));
+
+    std::function<int(std::string_view, std::string_view)> lcs =
+        [&](std::string_view s1, std::string_view s2) -> int {
+      if (s1.empty() || s2.empty()) {
+        return 0;
+      }
+      auto &ans = memo[s1.size() - 1][s2.size() - 1];
+      if (ans == -1) {
+        if (s1[0] == s2[0]) {
+          ans = 1 + lcs(s1.substr(1), s2.substr(1));
+        } else {
+          ans = std::max(lcs(s1, s2.substr(1)), lcs(s1.substr(1), s2));
+        }
+      }
+      return ans;
+    };
+
+    return lcs(text1, text2);
+  }
+};
+
+} // namespace memo
+
+namespace dp {
+
+// Time: O(NM)
+// Space: O(NM)
+class Solution {
+public:
+  static int longestCommonSubsequence(std::string text1, std::string text2) {
+    const int n = text1.size(), m = text2.size();
+    std::vector dp(n + 1, std::vector<int>(m + 1));
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j <= m; ++j) {
         if (text1[i - 1] == text2[j - 1]) {
           dp[i][j] = 1 + dp[i - 1][j - 1];
         } else {
-          dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+          dp[i][j] = std::max(dp[i][j - 1], dp[i - 1][j]);
         }
       }
     }
     return dp[n][m];
   }
 };
+
+} // namespace dp
+
+namespace opt {
+
+// Time: O(NM)
+// Space: O(min(N,M))
+class Solution {
+public:
+  static int longestCommonSubsequence(std::string text1, std::string text2) {
+    if (text1.size() < text2.size()) {
+      std::swap(text1, text2);
+    }
+    std::vector<int> dp(text2.size() + 1);
+    for (int i = 1; i <= std::ssize(text1); ++i) {
+      int prev = 0;
+      for (int j = 1; j <= std::ssize(text2); ++j) {
+        if (text1[i - 1] == text2[j - 1]) {
+          dp[j] = 1 + std::exchange(prev, dp[j]);
+        } else {
+          prev = std::exchange(dp[j], std::max(dp[j - 1], dp[j]));
+        }
+      }
+    }
+    return dp.back();
+  }
+};
+
+} // namespace opt
--- solutions/longest-common-subsequence/test.cpp
+++ solutions/longest-common-subsequence/test.cpp
@@ -25,10 +25,32 @@ TEST_CASE("Simple") {
           .text2 = "def",
           .expected = 0,
       },
+      {
+          .text1 = "psnw",
+          .text2 = "vozsh",
+          .expected = 1,
+      },
   };
 
-  for (const auto &[text1, text2, expected] : test_cases) {
-    const auto actual = Solution::longestCommonSubsequence(text1, text2);
-    REQUIRE(expected == actual);
+  SECTION("Memoization") {
+    for (const auto &[text1, text2, expected] : test_cases) {
+      const auto actual =
+          memo::Solution::longestCommonSubsequence(text1, text2);
+      REQUIRE(expected == actual);
+    }
+  }
+
+  SECTION("Tabulation") {
+    for (const auto &[text1, text2, expected] : test_cases) {
+      const auto actual = dp::Solution::longestCommonSubsequence(text1, text2);
+      REQUIRE(expected == actual);
+    }
+  }
+
+  SECTION("Optimized") {
+    for (const auto &[text1, text2, expected] : test_cases) {
+      const auto actual = opt::Solution::longestCommonSubsequence(text1, text2);
+      REQUIRE(expected == actual);
+    }
   }
 }
